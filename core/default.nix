ba8a932356d4d3d4d8f0cd470a7c4858a80a9075
onelocked <onelock@mail.com>
2026-01-28 19:59:05 +0000

    auto importer

--- core/default.nix
+++ core/default.nix
@@ -1,33 +1,35 @@
 { lib, ... }:
+
 let
   mapAttrsMaybe =
     f: attrs:
-    lib.pipe attrs [
-      (lib.mapAttrsToList f)
-      (builtins.filter (x: x != null))
-      builtins.listToAttrs
-    ];
+      lib.pipe attrs [
+        (lib.mapAttrsToList f)
+        (builtins.filter (x: x != null))
+        builtins.listToAttrs
+      ];
+
   forAllNixFiles =
     dir: f:
-    if builtins.pathExists dir then
-      lib.pipe dir [
-        builtins.readDir
-        (mapAttrsMaybe (
-          fn: type:
-          if type == "regular" then
-            let
-              name = lib.removeSuffix ".nix" fn;
-            in
-            lib.nameValuePair name (f "${dir}/${fn}")
-          else if type == "directory" && builtins.pathExists "${dir}/${fn}/default.nix" then
-            lib.nameValuePair fn (f "${dir}/${fn}")
-          else
-            null
-        ))
-      ]
-    else
-      { };
+      if builtins.pathExists dir then
+        lib.pipe dir [
+          builtins.readDir
+          (mapAttrsMaybe (
+            fn: type:
+              if fn == "default.nix" then
+                null  # skip default.nix
+              else if type == "regular" then
+                let name = lib.removeSuffix ".nix" fn; in
+                lib.nameValuePair name (f "${dir}/${fn}")
+              else if type == "directory" && builtins.pathExists "${dir}/${fn}/default.nix" then
+                lib.nameValuePair fn (f "${dir}/${fn}")
+              else
+                null
+          ))
+        ]
+      else
+        { };
 in
 {
-  flake.onelockCore = forAllNixFiles ./. (fn: fn);
+  flake.nixosModules.core = forAllNixFiles ./. (fn: fn);
 }
