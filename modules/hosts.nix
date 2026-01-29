38fa8afe0ae668efddfcac2e6fba92c9228b42e8
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-29 15:54:14 +0000

    [hosts] add hosts file for future

---
+++ modules/hosts.nix
@@ -0,0 +1,8 @@
+{ inputs, ... }:
+{
+  flake.nixosConfigurations.onelock = inputs.nixpkgs.lib.nixosSystem {
+    system = "x86_64-linux";
+    modules = with inputs.self.modules.nixos; [
+    ];
+  };
+}
