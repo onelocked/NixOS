feed0c1822153273e602302009ebe34267929ea5
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-30 23:20:54 +0000

    [quickshell] adapt quickshell package

--- modules/desktop/default.nix
+++ modules/desktop/default.nix
@@ -5,6 +5,7 @@
       niri
       sddm
       vicinae
+      quickshell
     ];
 
   };
---
+++ modules/desktop/quickshell.nix
@@ -0,0 +1,53 @@
+{ inputs, ... }:
+{
+  flake.modules.nixos.quickshell =
+    {
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      quickshell-flake = inputs.quickshell.packages.${pkgs.stdenv.hostPlatform.system}.default;
+      quickshell-deps =
+        with pkgs;
+        [
+          imagemagick
+          wlsunset
+          python3
+
+        ]
+        ++ (with pkgs.kdePackages; [
+          qt6ct
+          qtbase
+          qtmultimedia
+        ]);
+
+      qmlImportPath = lib.makeSearchPath "lib/qt6/qml" quickshell-deps;
+
+      quickshell = pkgs.symlinkJoin {
+        name = "quickshell";
+        paths = [ quickshell-flake ] ++ quickshell-deps;
+
+        buildInputs = [ pkgs.makeWrapper ];
+
+        postBuild = ''
+          wrapProgram $out/bin/quickshell \
+            --prefix PATH : ${lib.makeBinPath quickshell-deps} \
+            --prefix QML_IMPORT_PATH : ${qmlImportPath}
+        '';
+        meta = {
+          mainProgram = "quickshell";
+        };
+      };
+    in
+    {
+      programs.quickshell = {
+        enable = true;
+        package = quickshell;
+        systemd = {
+          enable = true;
+          target = "graphical-session.target";
+        };
+      };
+    };
+}
