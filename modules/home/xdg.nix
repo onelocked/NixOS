4eed6812c11d5f8ae631f2d61115bc77e2d6dfe4
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-30 17:18:30 +0000

    fix paths in xdg.nix

--- modules/home/xdg.nix
+++ modules/home/xdg.nix
@@ -6,16 +6,16 @@
       userDirs = {
         enable = true;
         createDirectories = true;
-        download = self.variables.homedir + "Downloads";
-        pictures = self.variables.homedir + "Pictures";
-        videos = self.variables.homedir + "Videos";
+        download = self.variables.homedir + "/Downloads";
+        pictures = self.variables.homedir + "/Pictures";
+        videos = self.variables.homedir + "/Videos";
         documents = self.variables.homedir + "/Documents";
         desktop = null;
         music = null;
         publicShare = null;
         templates = null;
         extraConfig = {
-          XDG_PROJECTS_DIR = self.variables.homedir + "Development";
+          XDG_PROJECTS_DIR = self.variables.homedir + "/Development";
           XDG_BIN_HOME = self.variables.homedir + "/.local/bin";
         };
       };
