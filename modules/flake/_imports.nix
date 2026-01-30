f1e837ea8f6d45f3ce288f34031b99644816b2c5
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-30 16:06:56 +0000

    no need for these imports
    its top level now

--- modules/flake/_imports.nix
+++
@@ -1,9 +0,0 @@
-{ inputs, ... }:
-{
-  flake.modules.nixos.core = {
-    imports = [ inputs.self.modules.generic.constants ];
-  };
-  flake.modules.homeManager.onelock = {
-    imports = [ inputs.self.modules.generic.constants ];
-  };
-}
