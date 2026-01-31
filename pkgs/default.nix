468c35a13e2520cfff78c6eddc989b535d9fd1a2
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-31 18:51:03 +0000

    [packages] re-enable wayland-ocr

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -6,7 +6,7 @@ in
   sddm-onelock = _ ./sddm-themes { theme = "onelock"; };
   apple-nerd-fonts = _ ./apple-nerd-fonts { inherit pkgs; };
   sunshine-xdg = _ ./sunshine/package.nix { };
-  # wayland-ocr = _ ./wayland-ocr { };
+  wayland-ocr = _ ./wayland-ocr { };
   # win2xcur = _ ./win2xcur { };
   # allCursors = (import ./cursors { inherit (pkgs) lib pkgs; });
 }
