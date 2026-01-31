c8e74900edf573c562ba0589fbac68c518646cf7
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-31 21:52:22 +0000

    [nix] cleanup nix settings

---
+++ modules/nixos/core/locale.nix
@@ -0,0 +1,20 @@
+{ self, ... }:
+{
+  flake.modules.nixos.core = {
+    # Timezone and locale
+    time.timeZone = "${self.variables.timezone}";
+    i18n.defaultLocale = "${self.variables.locale}";
+    i18n.extraLocaleSettings = {
+      LC_ADDRESS = "${self.variables.locale}";
+      LC_IDENTIFICATION = "${self.variables.locale}";
+      LC_MEASUREMENT = "${self.variables.locale}";
+      LC_MONETARY = "${self.variables.locale}";
+      LC_NAME = "${self.variables.locale}";
+      LC_NUMERIC = "${self.variables.locale}";
+      LC_PAPER = "${self.variables.locale}";
+      LC_TELEPHONE = "${self.variables.locale}";
+      LC_TIME = "${self.variables.locale}";
+    };
+    system.stateVersion = "25.11";
+  };
+}
---
+++ modules/nixos/core/nix.nix
@@ -0,0 +1,49 @@
+{ self, inputs, ... }:
+{
+  flake.modules.nixos.core =
+    { pkgs, lib, ... }:
+    {
+      nix = {
+        settings = {
+          trusted-users = [
+            "root"
+            self.variables.username
+          ];
+          auto-optimise-store = true;
+          substituters = [
+            "https://cache.nixos.org/"
+            "https://cachix.cachix.org"
+          ];
+          trusted-public-keys = [
+            "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+            "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
+          ];
+          experimental-features = [
+            "nix-command"
+            "flakes"
+          ];
+          use-xdg-base-directories = false;
+          warn-dirty = false;
+          keep-outputs = true;
+          keep-derivations = true;
+        };
+        optimise.automatic = true;
+        package = pkgs.nixVersions.latest;
+      };
+      nixpkgs = {
+        overlays = [
+          (
+            final: prev:
+            import ../../../pkgs {
+              pkgs = final;
+              inherit inputs;
+            }
+          )
+        ];
+        hostPlatform = lib.mkDefault "x86_64-linux";
+        config = {
+          allowUnfree = false;
+        };
+      };
+    };
+}
