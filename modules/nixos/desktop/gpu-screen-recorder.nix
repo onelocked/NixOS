5a8946469e8cdefe77b86d865dfe146488ac5e47
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-31 01:29:39 +0000

    [desktop] add gpu screen recorder

---
+++ modules/nixos/desktop/gpu-screen-recorder.nix
@@ -0,0 +1,13 @@
+{
+  flake.modules.nixos.desktop =
+    { pkgs, lib, ... }:
+    {
+      environment.systemPackages = with pkgs; [ gpu-screen-recorder ];
+      security.wrappers.gsr-kms-server = {
+        source = lib.getExe' pkgs.gpu-screen-recorder "gsr-kms-server";
+        capabilities = "cap_sys_admin+ep";
+        owner = "root";
+        group = "root";
+      };
+    };
+}
