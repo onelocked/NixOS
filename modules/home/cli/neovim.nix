4d0758762ebfb1a42a94d2672f6cfcb9fe10d72a
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-31 18:24:35 +0000

    [neovim] add neovim package

---
+++ modules/home/cli/neovim.nix
@@ -0,0 +1,12 @@
+{ inputs, ... }:
+{
+  flake.modules.homeManager.cli =
+    { pkgs, ... }:
+    {
+
+      home.packages = with pkgs; [
+        inputs.onevix.packages.${stdenv.hostPlatform.system}.default
+      ];
+
+    };
+}
