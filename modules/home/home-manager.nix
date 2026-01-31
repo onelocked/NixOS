01b2482fab3330231ce4aadb01338ab78fc03322
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-31 21:35:30 +0000

    [HM] move home manager stuff into host.nix for better maintainability

--- modules/home/home-manager.nix
+++ modules/home/home-manager.nix
@@ -30,24 +30,5 @@
         useUserPackages = true;
         backupFileExtension = "backup";
       };
-      home-manager.users.${self.variables.username} = {
-        imports = with inputs.self.modules.homeManager; [
-          xdg
-          cli
-          vicinae
-          quickshell
-          theming
-          zen-browser
-          media
-          jellyfin-desktop
-          ghostty
-        ];
-        home.username = self.variables.username;
-        home.homeDirectory = self.variables.homedir;
-        home.stateVersion = "25.11";
-        home.sessionVariables = {
-          EDITOR = "nvim";
-        };
-      };
     };
 }
--- modules/host.nix
+++ modules/host.nix
@@ -1,15 +1,40 @@
-{ inputs, ... }:
+{ self, inputs, ... }:
 {
   flake.nixosConfigurations = {
     NixOS = inputs.nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = with inputs.self.modules.nixos; [
-        hardware
-        amdgpu
-        core
-        desktop
-        home-manager
-      ];
+      modules =
+        with inputs.self.modules.nixos;
+        [
+          hardware
+          amdgpu
+          core
+          desktop
+          home-manager
+        ]
+        ++ [
+          {
+            home-manager.users.${self.variables.username} = {
+              imports = with inputs.self.modules.homeManager; [
+                xdg
+                cli
+                vicinae
+                quickshell
+                theming
+                zen-browser
+                media
+                jellyfin-desktop
+                ghostty
+              ];
+              home.username = self.variables.username;
+              home.homeDirectory = self.variables.homedir;
+              home.stateVersion = "25.11";
+              home.sessionVariables = {
+                EDITOR = "nvim";
+              };
+            };
+          }
+        ];
     };
   };
 }
