913c4469893b70a09041712dfbefea0e5275246c
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-31 14:17:01 +0000

    [media] add media packages

---
+++ modules/home/desktop/media/default.nix
@@ -0,0 +1,15 @@
+{
+  flake.modules.homeManager.media =
+    { pkgs, ... }:
+    {
+      home.packages = with pkgs; [
+        #Programs
+        vesktop
+        moonlight-qt
+        telegram-desktop
+        parsec-bin
+
+      ];
+
+    };
+}
