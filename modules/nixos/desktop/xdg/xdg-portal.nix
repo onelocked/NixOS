5cc9899487d68631497d9d48dc48686d8608e4f2
onelocked <180497738+onelocked@users.noreply.github.com>
2026-01-31 01:44:55 +0000

    add xdg-portal

---
+++ modules/nixos/desktop/xdg/xdg-portal.nix
@@ -0,0 +1,99 @@
+{
+  flake.modules.nixos.desktop =
+    { pkgs, lib, ... }:
+    let
+      inherit (lib) mkForce;
+    in
+    {
+      xdg.portal = {
+        enable = true;
+        extraPortals = with pkgs; [
+          xdg-desktop-portal-termfilechooser
+        ];
+        xdgOpenUsePortal = true;
+        wlr.enable = false;
+      };
+      xdg.portal.config = {
+        common = {
+          "org.freedesktop.impl.portal.FileChooser" = mkForce [
+            "termfilechooser"
+          ];
+          "org.freedesktop.impl.portal.Secret" = mkForce [
+            "gnome-keyring"
+          ];
+        };
+        niri = {
+          "org.freedesktop.impl.portal.FileChooser" = mkForce [
+            "termfilechooser"
+          ];
+          "org.freedesktop.impl.portal.Secret" = mkForce [
+            "gnome-keyring"
+          ];
+        };
+      };
+      xdg.terminal-exec = {
+        enable = true;
+        settings = {
+          default = [ "ghostty.desktop" ];
+        };
+      };
+      home-manager.sharedModules = [
+        {
+          xdg.configFile = {
+            "xdg-desktop-portal-termfilechooser/config".text = ''
+              [filechooser]
+              cmd=yazi-wrapper.sh
+              default_dir=$HOME
+              open_mode=default
+              save_mode=last
+            '';
+            "xdg-desktop-portal-termfilechooser/yazi-wrapper.sh".text = ''
+              #!/usr/bin/env sh
+
+              set -e
+
+              if [ "$6" -ge 4 ]; then
+                  set -x
+              fi
+
+              multiple="$1"
+              directory="$2"
+              save="$3"
+              path="$4"
+              out="$5"
+
+              cmd="yazi"
+              termcmd="ghostty -e"
+
+              if [ "$save" = "1" ]; then
+                  set -- --chooser-file="$out" "$path"
+              elif [ "$directory" = "1" ]; then
+                  set -- --chooser-file="$out" --cwd-file="$out"".1" "$path"
+              elif [ "$multiple" = "1" ]; then
+                  set -- --chooser-file="$out" "$path"
+              else
+                  set -- --chooser-file="$out" "$path"
+              fi
+
+              command="$termcmd $cmd"
+              for arg in "$@"; do
+                  escaped=$(printf "%s" "$arg" | sed 's/"/\\"/g')
+                  command="$command \"$escaped\""
+              done
+
+              sh -c "$command"
+
+              if [ "$directory" = "1" ]; then
+                  if [ ! -s "$out" ] && [ -s "$out"".1" ]; then
+                      cat "$out"".1" > "$out"
+                      rm "$out"".1"
+                  else
+                      rm "$out"".1"
+                  fi
+              fi
+            '';
+          };
+        }
+      ];
+    };
+}
