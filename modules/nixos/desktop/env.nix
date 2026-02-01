c032dc26f2611c0925e386816d4ee2e12ed35d71
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-01 20:53:39 +0000

    [nix] add environment variables

---
+++ modules/nixos/desktop/env.nix
@@ -0,0 +1,32 @@
+{
+  flake.modules.nixos.desktop = {
+    environment.sessionVariables = {
+      XDG_CURRENT_DESKTOP = "niri";
+      XDG_SESSION_DESKTOP = "niri";
+      XDG_SESSION_TYPE = "wayland";
+
+      GDK_BACKEND = "wayland";
+      NIXOS_OZONE_WL = "1";
+      ELECTRON_OZONE_PLATFORM_HINT = "auto";
+      MOZ_ENABLE_WAYLAND = "1";
+
+      OZONE_PLATFORM = "wayland";
+      EGL_PLATFORM = "wayland";
+      CLUTTER_BACKEND = "wayland";
+      SDL_VIDEODRIVER = "wayland";
+
+      QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+      QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+      QT_ENABLE_HIGHDPI_SCALING = "1";
+
+      QS_ICON_THEME = "Papirus-Dark";
+      GTK_THEME = "adw-gtk3-dark";
+      GTK_USE_PORTAL = "1";
+
+      WLR_RENDERER_ALLOW_SOFTWARE = "0";
+      WLR_RENDERER = "vulkan";
+
+      LIBVA_DRIVER_NAME = "radeonsi";
+    };
+  };
+}
