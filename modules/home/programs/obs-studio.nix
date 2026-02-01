1e9997d9228629f40eb501efe76dc0b9d083b35d
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-01 02:50:08 +0000

    [obs-studio] wrap the package with plugins and auto-start with systemd

--- modules/home/programs/obs-studio.nix
+++ modules/home/programs/obs-studio.nix
@@ -1,19 +1,43 @@
 {
   flake.modules.homeManager.media =
-    { pkgs, ... }:
+    { pkgs, lib, ... }:
+    let
+      obs-wrapped = (
+        pkgs.wrapOBS {
+          plugins = with pkgs.obs-studio-plugins; [
+            distroav
+            wlrobs
+            obs-vaapi
+            obs-vkcapture
+            obs-pipewire-audio-capture
+          ];
+        }
+      );
+    in
     {
-      systemd.user.tmpfiles.rules = [
-        "R %h/.config/obs-studio/.sentinel"
-      ];
-      programs.obs-studio = {
-        enable = true;
-        plugins = with pkgs.obs-studio-plugins; [
-          distroav
-          wlrobs
-          obs-vaapi
-          obs-vkcapture
-          obs-pipewire-audio-capture
+      home.packages = [ obs-wrapped ];
+
+      systemd.user = {
+        tmpfiles.rules = [
+          "R %h/.config/obs-studio/.sentinel"
         ];
+
+        services."obs-startup" = {
+          Unit = {
+            Description = "OBS Startup";
+            After = [ "quickshell.service" ];
+          };
+
+          Service = {
+            Type = "simple";
+            ExecStartPre = "${pkgs.coreutils}/bin/sleep 1";
+            ExecStart = lib.getExe obs-wrapped;
+          };
+
+          Install = {
+            WantedBy = [ "quickshell.service" ];
+          };
+        };
       };
     };
 }
