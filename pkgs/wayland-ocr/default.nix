ca630467d15cb6a08aa98fbf5d26874f58168c0f
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-01 01:37:09 +0000

    [wayland-ocr] make the script more readable

--- pkgs/wayland-ocr/default.nix
+++ pkgs/wayland-ocr/default.nix
@@ -12,14 +12,16 @@ let
   _ = lib.getExe;
 in
 writeShellScriptBin "ocr" ''
-  if selection="$(${_ slurp})"; then
-    if [ -n "$selection" ]; then
-      ${_ grim} -g "$selection" -t ppm - | ${_ tesseract5} -l ${langs} - - | ${wl-clipboard-rs}/bin/wl-copy
-      ${_ libnotify} -- "OCR Copied to clipboard"
-    else
-      ${_ libnotify} -- "OCR Cancelled by user"
-    fi
-  else
+  selection="$(${_ slurp} || true)"
+
+  if [ -z "$selection" ]; then
     ${_ libnotify} -- "OCR Cancelled by user"
+    exit 0
   fi
+
+  ${_ grim} -g "$selection" -t ppm - \
+    | ${_ tesseract5} -l ${langs} - - \
+    | ${wl-clipboard-rs}/bin/wl-copy
+
+  ${_ libnotify} -- "OCR copied to clipboard"
 ''
