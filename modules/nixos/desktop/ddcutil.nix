08e45aaa97f4457ca0c8ff6df041e7f2528ffef9
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-02 22:24:27 +0000

    [ddcutil] enable to control monitor brightness

---
+++ modules/nixos/desktop/ddcutil.nix
@@ -0,0 +1,17 @@
+{ self, ... }:
+{
+  flake.modules.nixos.desktop =
+    { pkgs, ... }:
+    {
+      environment.systemPackages = with pkgs; [
+        ddcutil
+      ];
+
+      services.udev.extraRules = ''
+        KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
+      '';
+      boot.kernelModules = [ "i2c-dev" ];
+      users.groups.i2c = { };
+      users.users.${self.variables.username}.extraGroups = [ "i2c" ];
+    };
+}
