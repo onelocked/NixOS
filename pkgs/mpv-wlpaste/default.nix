f155d88265521d6a2f679e4841593109e458a307
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-03 03:16:50 +0000

    [mpv-wlpaste] another improvement to the script

--- pkgs/mpv-wlpaste/default.nix
+++ pkgs/mpv-wlpaste/default.nix
@@ -2,18 +2,18 @@
   writeShellScriptBin,
   wl-clipboard-rs,
   mpv,
+  coreutils,
 }:
 
 writeShellScriptBin "mpv-wlpaste" ''
-  url="$(${wl-clipboard-rs}/bin/wl-paste || true)"
-
-  [ -z "$url" ] && exit 0
-
+  url=$(${wl-clipboard-rs}/bin/wl-paste | ${coreutils}/bin/tr -d '[:space:]')
+  if [ -z "$url" ]; then
+    exit 0
+  fi
   case "$url" in
-    *tiktok.com/*\?*)
+    *tiktok.com*)
       url="''${url%%\?*}"
       ;;
   esac
-
   exec ${mpv}/bin/mpv "$url"
 ''
