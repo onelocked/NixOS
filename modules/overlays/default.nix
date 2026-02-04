1c336f7771555889f4f1a85a5f2708a41b57cee5
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-04 22:23:22 +0000

    [overlays] cleaner and more readable

--- modules/overlays/default.nix
+++ modules/overlays/default.nix
@@ -2,16 +2,23 @@
 {
   flake.modules.nixos.overlays = {
     nixpkgs.overlays = [
-      (final: prev: {
-        wl-clipboard = prev.wl-clipboard-rs;
-        opencode = inputs.opencode.packages.${final.stdenv.hostPlatform.system}.default;
-        ghostty = inputs.ghostty.packages.${final.stdenv.hostPlatform.system}.default;
-        niri = inputs.niri-flake.packages.${final.stdenv.hostPlatform.system}.niri-unstable;
-        neovim = inputs.onevix.packages.${final.stdenv.hostPlatform.system}.default;
-        lan-mouse = inputs.lan-mouse.packages.${final.stdenv.hostPlatform.system}.default;
-        xwayland-satellite =
-          inputs.niri-flake.packages.${final.stdenv.hostPlatform.system}.xwayland-satellite-unstable;
-      })
+      (
+        final: prev:
+        let
+          system = final.stdenv.hostPlatform.system;
+          pkg = input: name: inputs.${input}.packages.${system}.${name};
+        in
+        {
+          wl-clipboard = prev.wl-clipboard-rs;
+
+          opencode = pkg "opencode" "default";
+          ghostty = pkg "ghostty" "default";
+          niri = pkg "niri-flake" "niri-unstable";
+          neovim = pkg "onevix" "default";
+          lan-mouse = pkg "lan-mouse" "default";
+          xwayland-satellite = pkg "niri-flake" "xwayland-satellite-unstable";
+        }
+      )
     ];
   };
 }
