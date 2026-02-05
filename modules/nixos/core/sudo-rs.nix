b6956420a6afafa2588626c4279a1824c7b1616c
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-05 22:21:55 +0000

    [nix] replace sudo with sudo-rs

---
+++ modules/nixos/core/sudo-rs.nix
@@ -0,0 +1,12 @@
+{
+  flake.modules.nixos.core = {
+    security = {
+      sudo.enable = false;
+      sudo-rs = {
+        enable = true;
+        wheelNeedsPassword = true;
+        execWheelOnly = true;
+      };
+    };
+  };
+}
