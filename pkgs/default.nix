bb869030732e0961fca38b7f9651aa3985df93a2
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-05 02:08:49 +0000

    [pkgs] remove redudant and old packages that are not used

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -5,10 +5,8 @@ in
 {
   sddm-onelock = _ ./sddm-themes { theme = "onelock"; };
   apple-nerd-fonts = _ ./apple-nerd-fonts { inherit pkgs; };
-  sunshine-xdg = _ ./sunshine/package.nix { };
   wayland-ocr = _ ./wayland-ocr { };
   launcher = _ ./niri-launcher { };
   mpv-wlpaste = _ ./mpv-wlpaste { };
-  # win2xcur = _ ./win2xcur { };
   # allCursors = (import ./cursors { inherit (pkgs) lib pkgs; });
 }
