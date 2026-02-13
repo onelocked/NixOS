08e78ff7961bc8ae466381f3c070da892528b080
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-13 19:56:29 +0000

    [kde-polkit] remove kde-polkit because noctalia handles it

--- modules/nixos/desktop/niri.nix
+++ modules/nixos/desktop/niri.nix
@@ -14,18 +14,5 @@
         enable = true;
         package = pkgs.xwayland-satellite;
       };
-      systemd.user.services.niri-flake-polkit = {
-        description = "PolicyKit Authentication by KDE";
-        wantedBy = [ "niri.service" ];
-        after = [ "graphical-session.target" ];
-        partOf = [ "graphical-session.target" ];
-        serviceConfig = {
-          Type = "simple";
-          ExecStart = "${pkgs.kdePackages.polkit-kde-agent-1}/libexec/polkit-kde-authentication-agent-1";
-          Restart = "on-failure";
-          RestartSec = 1;
-          TimeoutStopSec = 10;
-        };
-      };
     };
 }
