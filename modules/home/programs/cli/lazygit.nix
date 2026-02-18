a4a897bdd3ff0d4394c5573319345eb0def5a5f9
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-18 17:20:57 +0000

    [git] better config

--- modules/home/programs/cli/lazygit.nix
+++ modules/home/programs/cli/lazygit.nix
@@ -3,52 +3,50 @@
   flake.modules.homeManager.cli =
     { pkgs, ... }:
     {
-      home.shellAliases = {
-        lg = "lazygit";
+      programs.git = {
+        enable = true;
+        settings = {
+          user = {
+            name = "${self.variables.username}ed";
+            email = "${self.variables.email}";
+          };
+          interactive = {
+            diffFilter = "delta --color-only";
+          };
+          core = {
+            editor = "$EDITOR";
+            pager = "delta";
+          };
+          delta = {
+            navigate = true;
+            dark = true;
+            line-numbers = true;
+            hyperlinks = true;
+          };
+          merge = {
+            conflictStyle = "zdiff3";
+          };
+          diff = {
+            colorMoved = "default";
+          };
+          init.defaultBranch = "main";
+          advice.objectNameWarning = false;
+          pull.rebase = true;
+          safe.directory = "/tmp"; # needed for ago.sh
+        };
       };
       programs.lazygit = {
         enable = true;
         enableNushellIntegration = true;
       };
       home = {
+        shellAliases = {
+          lg = "lazygit";
+        };
         packages = with pkgs; [
           delta
           gh-dash
         ];
-        file = {
-          ".gitconfig".text = ''
-            [user]
-            	name = onelocked
-            	email = ${self.variables.email}
-
-            [core]
-                pager = delta
-
-            [interactive]
-                diffFilter = delta --color-only
-            [delta]
-                navigate = true  # use n and N to move between diff sections
-                dark = true      # or light = true, or omit for auto-detection
-                line-numbers = true
-                hyperlinks = true
-
-            [merge]
-                conflictStyle = zdiff3
-
-            [diff]
-                colorMoved = default
-          '';
-        };
-      };
-      programs.git = {
-        enable = true;
-        settings = {
-          core.editor = "$EDITOR";
-          init.defaultBranch = "main";
-          advice.objectNameWarning = false;
-          pull.rebase = true;
-          safe.directory = "/tmp"; # needed for ago.sh
-        };
       };
     };
 }
