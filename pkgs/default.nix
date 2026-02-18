386f20645ea2bc757491efff47daf3ffd37df814
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-18 00:20:22 +0000

    [nix] rename files and change directory structure for pkgs

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,10 +3,9 @@ let
   _ = pkgs.callPackage;
 in
 {
-  apple-nerd-fonts = _ ./apple-nerd-fonts { inherit pkgs; };
-  wayland-ocr = _ ./wayland-ocr { };
-  niri-launcher = _ ./niri-launcher { };
-  mpv-wlpaste = _ ./mpv-wlpaste { };
-  amdgpu_top = _ ./amdgpu_top { };
-  # allCursors = (import ./cursors { inherit (pkgs) lib pkgs; });
+  apple-nerd-fonts = _ ./apple-nerd-fonts.nix { inherit pkgs; };
+  wayland-ocr = _ ./wayland-ocr.nix { };
+  niri-launcher = _ ./niri-launcher.nix { };
+  mpv-wlpaste = _ ./mpv-wlpaste.nix { };
+  amdgpu_top = _ ./amdgpu_top.nix { };
 }
