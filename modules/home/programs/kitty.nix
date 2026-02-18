ecaae1b86a8912eab5fba4dab8ec46d9686c6d47
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-18 22:43:21 +0000

    [kitty] add initial kitty config

---
+++ modules/home/programs/kitty.nix
@@ -0,0 +1,49 @@
+{
+  flake.modules.homeManager.kitty = {
+    programs.kitty = {
+      enable = true;
+      settings = {
+        include = "./themes/noctalia.conf";
+
+        font_family = "Liga SFMono Semibold";
+        bold_font = "Liga SFMono Bold";
+        italic_font = "Liga SFMono Semibold Italic";
+        bold_italic_font = "Liga SFMono Bold Italic";
+        font_size = "15";
+
+
+
+        adjust_line_height = "0";
+        adjust_column_width = "0";
+
+        sync_to_monitor = "yes";
+        remember_window_position = "no";
+
+        draw_minimal_borders = "yes";
+        window_margin_width = "2";
+        placement_strategy = "center";
+        update_check_interval = "24";
+        allow_hyperlinks = "yes";
+
+
+        scrollback_lines = "10000";
+        wheel_scroll_multiplier = "5.0";
+
+        strip_trailing_spaces = "smart";
+        hide_window_decorations = "yes";
+
+        enable_audio_bell = "no";
+        visual_bell_duration = "0.0";
+        repaint_delay = "10";
+
+        confirm_os_window_close = "0";
+
+        cursor_trail = "1";
+        cursor_trail_decay = "0.1 0.2";
+        cursor_shape = "beam";
+        cursor_blink_interval = "0.5";
+        cursor_stop_blinking_after = "15.0";
+      };
+    };
+  };
+}
--- modules/host.nix
+++ modules/host.nix
@@ -28,6 +28,7 @@
                 media
                 jellyfin-desktop
                 ghostty
+                kitty
               ];
             };
           }
