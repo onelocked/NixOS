21940ac4bd55efbdbbfd9cb1643c8e5d3b74cac7
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-18 20:46:27 +0000

    [termfilechooser] slightly improve config

--- modules/nixos/desktop/xdg/xdg-termfilechooser.nix
+++ modules/nixos/desktop/xdg/xdg-termfilechooser.nix
@@ -20,64 +20,65 @@
           };
         };
       };
-    homeManager.state = {
-      xdg.configFile = {
-        "xdg-desktop-portal-termfilechooser/config".text = ''
-          [filechooser]
-          cmd=yazi-wrapper.sh
-          default_dir=${self.variables.homedir}/Downloads
-          open_mode=suggested
-          save_mode=default
-        '';
-        "xdg-desktop-portal-termfilechooser/yazi-wrapper.sh" = {
-          executable = true;
-          text = # bash
-            ''
-              #!/usr/bin/env sh
-              set -e
+    homeManager.state =
+      { pkgs, ... }:
+      {
+        xdg.configFile = {
+          "xdg-desktop-portal-termfilechooser/config".text = ''
+            [filechooser]
+            cmd=yazi-wrapper.sh
+            default_dir=${self.variables.homedir}/Downloads
+            open_mode=suggested
+            save_mode=default
+          '';
+          "xdg-desktop-portal-termfilechooser/yazi-wrapper.sh" = {
+            executable = true;
+            text = # bash
+              ''
+                #!/usr/bin/env sh
+                set -e
 
-              if [ "$6" -ge 4 ]; then
-                  set -x
-              fi
+                if [ "$6" -ge 4 ]; then
+                    set -x
+                fi
 
-              multiple="$1"
-              directory="$2"
-              save="$3"
-              path="$4"
-              out="$5"
+                multiple="$1"
+                directory="$2"
+                save="$3"
+                path="$4"
+                out="$5"
 
-              cmd="yazi"
-              termcmd="ghostty -e"
 
-              if [ "$save" = "1" ]; then
-                  set -- --chooser-file="$out" "$path"
-              elif [ "$directory" = "1" ]; then
-                  set -- --chooser-file="$out" --cwd-file="$out"".1" "$path"
-              elif [ "$multiple" = "1" ]; then
-                  set -- --chooser-file="$out" "$path"
-              else
-                  set -- --chooser-file="$out" "$path"
-              fi
+                if [ "$save" = "1" ]; then
+                    set -- --chooser-file="$out" "$path"
+                elif [ "$directory" = "1" ]; then
+                    set -- --chooser-file="$out" --cwd-file="$out"".1" "$path"
+                elif [ "$multiple" = "1" ]; then
+                    set -- --chooser-file="$out" "$path"
+                else
+                    set -- --chooser-file="$out" "$path"
+                fi
 
-              command="$termcmd $cmd"
-              for arg in "$@"; do
-                  escaped=$(printf "%s" "$arg" | sed 's/"/\\"/g')
-                  command="$command \"$escaped\""
-              done
+                command="${pkgs.kitty}/bin/kitty -e yazi"
 
-              sh -c "$command"
+                for arg in "$@"; do
+                    escaped=$(printf "%s" "$arg" | sed 's/"/\\"/g')
+                    command="$command \"$escaped\""
+                done
 
-              if [ "$directory" = "1" ]; then
-                  if [ ! -s "$out" ] && [ -s "$out"".1" ]; then
-                      cat "$out"".1" > "$out"
-                      rm "$out"".1"
-                  else
-                      rm "$out"".1"
-                  fi
-              fi
-            '';
+                sh -c "$command"
+
+                if [ "$directory" = "1" ]; then
+                    if [ ! -s "$out" ] && [ -s "$out"".1" ]; then
+                        cat "$out"".1" > "$out"
+                        rm "$out"".1"
+                    else
+                        rm "$out"".1"
+                    fi
+                fi
+              '';
+          };
         };
       };
-    };
   };
 }
