a3423e9fb3648ec87b267add7f35418c071e8a23
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-20 02:03:40 +0000

    [foot] better font size and padding

--- modules/home/programs/terminals/foot.nix
+++ modules/home/programs/terminals/foot.nix
@@ -7,12 +7,12 @@
         main = {
           include = "~/.config/foot/themes/noctalia";
 
-          font = "SFMono Nerd Font:style=Bold:size=12";
-          font-bold = "SFMono Nerd Font:style=Heavy:size=12";
-          font-italic = "SFMono Nerd Font:style=Bold Italic:size=12";
-          font-bold-italic = "SFMono Nerd Font:style=Heavy Italic:size=12";
+          font = "SFMono Nerd Font:style=Bold:size=13";
+          font-bold = "SFMono Nerd Font:style=Heavy:size=13";
+          font-italic = "SFMono Nerd Font:style=Bold Italic:size=13";
+          font-bold-italic = "SFMono Nerd Font:style=Heavy Italic:size=13";
 
-          pad = "6x6";
+          pad = "3x3";
           dpi-aware = "yes";
           bold-text-in-bright = "no";
           gamma-correct-blending = "no";
