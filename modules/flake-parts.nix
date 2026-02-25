eec917d6f03992446862adb7eff3c260226784b1
onelocked <180497738+onelocked@users.noreply.github.com>
2026-02-25 13:11:39 +0000

    [nix] add more systems

--- modules/flake-parts.nix
+++ modules/flake-parts.nix
@@ -4,7 +4,13 @@
     inputs.flake-parts.flakeModules.modules
     inputs.home-manager.flakeModules.home-manager
   ];
-  systems = [ "x86_64-linux" ];
+  systems = [
+    "x86_64-linux"
+    "aarch64-linux"
+    "x86_64-darwin"
+    "aarch64-darwin"
+  ];
+
   perSystem =
     { pkgs, ... }:
     {
