aee2cf0d81d70dbdb8a1055f3be0e213f3abaed1
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-08 13:10:02 +0000

    [workstation] slightly more cleaner

--- modules/hosts/mini-pc/workstation.nix
+++ modules/hosts/mini-pc/workstation.nix
@@ -4,35 +4,33 @@ let
   inherit (self.modules) nixos homeManager;
 in
 {
-  flake.nixosConfigurations = {
-    NixOS = mkSystem {
-      nixosModules = with nixos; [
-        mini-pc
-        onelock
-        core
-        overlays
-        home-manager
-        desktop
-      ];
-      homeModules = with homeManager; [
-        default
-        cli
-        theming
-        foot
-        vicinae
-        fuzzel
-        quickshell
-        zen-browser
-        media
-        jellyfin-desktop
-        jellyfin-tui
-        obs-studio
-        mpv
-        qview
-        fish
-        dott-tui
-        worktrunk
-      ];
-    };
+  flake.nixosConfigurations.NixOS = mkSystem {
+    nixosModules = with nixos; [
+      mini-pc
+      onelock
+      core
+      overlays
+      home-manager
+      desktop
+    ];
+    homeModules = with homeManager; [
+      default
+      cli
+      theming
+      foot
+      vicinae
+      fuzzel
+      quickshell
+      zen-browser
+      media
+      jellyfin-desktop
+      jellyfin-tui
+      obs-studio
+      mpv
+      qview
+      fish
+      dott-tui
+      worktrunk
+    ];
   };
 }
