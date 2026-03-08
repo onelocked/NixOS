77438cb2508aa4efe60a30d1634bdf8261efde46
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-08 13:33:22 +0000

    [grub] cleanup with inherit

--- modules/nixos/core/grub.nix
+++ modules/nixos/core/grub.nix
@@ -3,23 +3,27 @@
   flake.modules.nixos.core =
     { pkgs, ... }:
     {
-      boot.loader = {
-        efi = {
-          canTouchEfiVariables = true;
-          efiSysMountPoint = "/boot";
+      boot.loader =
+        let
+          inherit (self.variables) homedir;
+        in
+        {
+          efi = {
+            canTouchEfiVariables = true;
+            efiSysMountPoint = "/boot";
+          };
+          timeout = 5;
+          grub = {
+            enable = true;
+            device = "nodev";
+            efiSupport = true;
+            useOSProber = true;
+            gfxmodeEfi = "3440x1440";
+            gfxmodeBios = "3440x1440";
+            splashMode = "normal";
+            splashImage = homedir + "/Pictures/grub.png";
+            theme = pkgs.grub-theme;
+          };
         };
-        timeout = 5;
-        grub = {
-          enable = true;
-          device = "nodev";
-          efiSupport = true;
-          useOSProber = true;
-          gfxmodeEfi = "3440x1440";
-          gfxmodeBios = "3440x1440";
-          splashMode = "normal";
-          splashImage = "${self.variables.homedir}/Pictures/grub.png";
-          theme = pkgs.grub-theme;
-        };
-      };
     };
 }
