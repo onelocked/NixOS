c50e58de444b121261823b4e8d2b79944654a4df
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-09 00:58:38 +0000

    [xdg] big improvements to desktop entries

--- modules/home/programs/xdg.nix
+++ modules/home/programs/xdg.nix
@@ -1,54 +1,82 @@
 { self, ... }:
 {
-  flake.modules.homeManager.xdg = {
-    xdg =
-      let
-        inherit (self.variables) homedir;
-      in
-      {
-        enable = true;
-        userDirs = {
+  flake.modules.homeManager.xdg =
+    { pkgs, ... }:
+    {
+      xdg =
+        let
+          inherit (self.variables) homedir;
+        in
+        {
           enable = true;
-          createDirectories = true;
-          download = homedir + "/Downloads";
-          pictures = homedir + "/Pictures";
-          videos = homedir + "/Videos";
-          documents = homedir + "/Documents";
-          extraConfig = {
-            PROJECTS = homedir + "/Development";
+          userDirs = {
+            enable = true;
+            createDirectories = true;
+            download = homedir + "/Downloads";
+            pictures = homedir + "/Pictures";
+            videos = homedir + "/Videos";
+            documents = homedir + "/Documents";
+            extraConfig = {
+              PROJECTS = homedir + "/Development";
+            };
+            desktop = null;
+            music = null;
+            publicShare = null;
+            templates = null;
           };
-          desktop = null;
-          music = null;
-          publicShare = null;
-          templates = null;
-        };
 
-        desktopEntries = {
-          "yazi" = {
-            name = "Yazi";
-            noDisplay = true;
-          };
-          "qt5ct" = {
-            name = "Qt5 Configuration Tool";
-            noDisplay = true;
-          };
-          "qt6ct" = {
-            name = "Qt6 Configuration Tool";
-            noDisplay = true;
-          };
-          "footclient" = {
-            name = "Foot Client";
-            noDisplay = true;
-          };
-          "foot-server" = {
-            name = "Foot Server";
-            noDisplay = true;
-          };
-          "nixos-manual" = {
-            name = "NixOS Manual";
-            noDisplay = true;
+          desktopEntries = {
+            "yazi" = {
+              name = "Yazi";
+              noDisplay = true;
+            };
+            "qt5ct" = {
+              name = "Qt5 Configuration Tool";
+              noDisplay = true;
+            };
+            "qt6ct" = {
+              name = "Qt6 Configuration Tool";
+              noDisplay = true;
+            };
+            "footclient" = {
+              name = "Foot Client";
+              noDisplay = true;
+            };
+            "foot-server" = {
+              name = "Foot Server";
+              noDisplay = true;
+            };
+            "nixos-manual" = {
+              name = "NixOS Manual";
+              noDisplay = true;
+            };
+            "btop" = {
+              name = "btop++";
+              noDisplay = true;
+            };
+            "mpv" = {
+              name = "MPV";
+              exec = "${pkgs.mpv}/bin/mpv --player-operation-mode=pseudo-gui -- %U";
+              icon = "mpv";
+            };
+            "umpv" = {
+              name = "umpv Media Player";
+              noDisplay = true;
+            };
+            "vicinae" = {
+              name = "Vicinae";
+              noDisplay = true;
+            };
+            "nvim" = {
+              name = "Neovim Wrapper";
+              noDisplay = true;
+            };
+            "zen-twilight" = {
+              name = "Zen Browser";
+              icon = "zen-twilight";
+              exec = "${pkgs.niri-launcher}/bin/niri-launcher zen-twilight zen-twilight";
+            };
           };
         };
-      };
-  };
+    };
 }
