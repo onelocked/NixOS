ae61396c052b19fc273c574bc45ab346bee58ba9
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-11 19:49:06 +0000

    [xdg-desktop-entries] remove jellyfin-tui
    additionally moved the xdg desktop entries to a seperate file

---
+++ modules/nixos/desktop/xdg/xdg-desktop-entries.nix
@@ -0,0 +1,69 @@
+{
+  flake.modules.homeManager.xdg =
+    { pkgs, ... }:
+    {
+      xdg = {
+        desktopEntries = {
+          "yazi" = {
+            name = "Yazi";
+            noDisplay = true;
+          };
+          "qt5ct" = {
+            name = "Qt5 Configuration Tool";
+            noDisplay = true;
+          };
+          "qt6ct" = {
+            name = "Qt6 Configuration Tool";
+            noDisplay = true;
+          };
+          "footclient" = {
+            name = "Foot Client";
+            noDisplay = true;
+          };
+          "foot-server" = {
+            name = "Foot Server";
+            noDisplay = true;
+          };
+          "nixos-manual" = {
+            name = "NixOS Manual";
+            noDisplay = true;
+          };
+          "btop" = {
+            name = "btop++";
+            noDisplay = true;
+          };
+          "mpv" = {
+            name = "MPV";
+            exec = "${pkgs.mpv}/bin/mpv --player-operation-mode=pseudo-gui -- %U";
+            icon = "mpv";
+          };
+          "umpv" = {
+            name = "umpv Media Player";
+            noDisplay = true;
+          };
+          "vicinae" = {
+            name = "Vicinae";
+            noDisplay = true;
+          };
+          "nvim" = {
+            name = "Neovim Wrapper";
+            noDisplay = true;
+          };
+          "zen-twilight" = {
+            name = "Zen Browser";
+            icon = "zen-twilight";
+            exec = "${pkgs.niri-launcher}/bin/niri-launcher zen-twilight zen-twilight";
+          };
+          "org.jellyfin.JellyfinDesktop" = {
+            name = "Jellyfin";
+            icon = "org.jellyfin.JellyfinDesktop";
+            exec = "${pkgs.jellyfin-desktop}/bin/jellyfin-desktop --platform xcb";
+          };
+          "jellyfin-tui" = {
+            name = "jellyfin-tui";
+            noDisplay = true;
+          };
+        };
+      };
+    };
+}
--- modules/nixos/desktop/xdg/xdg.nix
+++ modules/nixos/desktop/xdg/xdg.nix
@@ -1,87 +1,27 @@
 { self, ... }:
 {
-  flake.modules.homeManager.xdg =
-    { pkgs, ... }:
-    {
-      xdg =
-        let
-          inherit (self.variables) homedir;
-        in
-        {
+  flake.modules.homeManager.xdg = {
+    xdg =
+      let
+        inherit (self.variables) homedir;
+      in
+      {
+        enable = true;
+        userDirs = {
           enable = true;
-          userDirs = {
-            enable = true;
-            createDirectories = true;
-            download = homedir + "/Downloads";
-            pictures = homedir + "/Pictures";
-            videos = homedir + "/Videos";
-            documents = homedir + "/Documents";
-            extraConfig = {
-              PROJECTS = homedir + "/Development";
-            };
-            desktop = null;
-            music = null;
-            publicShare = null;
-            templates = null;
+          createDirectories = true;
+          download = homedir + "/Downloads";
+          pictures = homedir + "/Pictures";
+          videos = homedir + "/Videos";
+          documents = homedir + "/Documents";
+          extraConfig = {
+            PROJECTS = homedir + "/Development";
           };
-
-          desktopEntries = {
-            "yazi" = {
-              name = "Yazi";
-              noDisplay = true;
-            };
-            "qt5ct" = {
-              name = "Qt5 Configuration Tool";
-              noDisplay = true;
-            };
-            "qt6ct" = {
-              name = "Qt6 Configuration Tool";
-              noDisplay = true;
-            };
-            "footclient" = {
-              name = "Foot Client";
-              noDisplay = true;
-            };
-            "foot-server" = {
-              name = "Foot Server";
-              noDisplay = true;
-            };
-            "nixos-manual" = {
-              name = "NixOS Manual";
-              noDisplay = true;
-            };
-            "btop" = {
-              name = "btop++";
-              noDisplay = true;
-            };
-            "mpv" = {
-              name = "MPV";
-              exec = "${pkgs.mpv}/bin/mpv --player-operation-mode=pseudo-gui -- %U";
-              icon = "mpv";
-            };
-            "umpv" = {
-              name = "umpv Media Player";
-              noDisplay = true;
-            };
-            "vicinae" = {
-              name = "Vicinae";
-              noDisplay = true;
-            };
-            "nvim" = {
-              name = "Neovim Wrapper";
-              noDisplay = true;
-            };
-            "zen-twilight" = {
-              name = "Zen Browser";
-              icon = "zen-twilight";
-              exec = "${pkgs.niri-launcher}/bin/niri-launcher zen-twilight zen-twilight";
-            };
-            "org.jellyfin.JellyfinDesktop" = {
-              name = "Jellyfin";
-              icon = "org.jellyfin.JellyfinDesktop";
-              exec = "${pkgs.jellyfin-desktop}/bin/jellyfin-desktop --platform xcb";
-            };
-          };
+          desktop = null;
+          music = null;
+          publicShare = null;
+          templates = null;
         };
-    };
+      };
+  };
 }
