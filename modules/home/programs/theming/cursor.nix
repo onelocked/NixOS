49991c74b0ea1756bf0208ab39a45e7a1134d3fe
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-12 02:58:04 +0000

    [cursors] add new cursors

--- modules/home/programs/theming/cursor.nix
+++ modules/home/programs/theming/cursor.nix
@@ -2,11 +2,17 @@
   flake.modules.homeManager.cursor =
     { pkgs, ... }:
     {
-      home.pointerCursor = {
-        gtk.enable = true;
-        x11.enable = true;
-        package = pkgs.bibata-cursors;
-        name = "Bibata-Modern-Ice";
+      home = {
+        packages = with pkgs; [
+          bareblood_cursor
+          hand-of-evil
+        ];
+        pointerCursor = {
+          gtk.enable = true;
+          x11.enable = true;
+          package = pkgs.bibata-cursors;
+          name = "Bibata-Modern-Ice";
+        };
       };
     };
 }
