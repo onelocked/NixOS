358df2121b962e1b2b4bb1ad72a0d0151cf5579a
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-12 19:35:16 +0000

    [vicinae] disable env var

--- modules/home/programs/launcher/vicinae.nix
+++ modules/home/programs/launcher/vicinae.nix
@@ -1,19 +1,12 @@
 {
-  flake.modules.homeManager.vicinae =
-    { lib, config, ... }:
-    {
-      programs.vicinae = {
+  flake.modules.homeManager.vicinae = {
+    programs.vicinae = {
+      enable = true;
+      useLayerShell = true;
+      systemd = {
         enable = true;
-        useLayerShell = true;
-        systemd = {
-          enable = true;
-          autoStart = true;
-        };
-      };
-      systemd.user.services.vicinae.Service = lib.mkIf config.programs.vicinae.enable {
-        Environment = lib.mkForce [
-          "QT_SCALE_FACTOR=1.5"
-        ];
+        autoStart = true;
       };
     };
+  };
 }
