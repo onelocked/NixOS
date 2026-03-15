698eb2c4bcc950ba68cd5314b1b42af3b3504365
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-15 14:25:10 +0000

    [direnv] minor imrpovement

--- modules/home/programs/cli/direnv.nix
+++ modules/home/programs/cli/direnv.nix
@@ -6,9 +6,11 @@
       enableBashIntegration = true;
       enableNushellIntegration = true;
       enableFishIntegration = true;
-    };
-    home.sessionVariables = {
-      DIRENV_WARN_TIMEOUT = "60s";
+      config = {
+        global = {
+          hide_env_diff = true;
+        };
+      };
     };
   };
 }
