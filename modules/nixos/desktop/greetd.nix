f0d444b784dafaf6cbd8be5d0885f07337968735
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-18 14:04:25 +0000

    [greetd] password less keyring authentication

--- modules/nixos/desktop/greetd.nix
+++ modules/nixos/desktop/greetd.nix
@@ -4,21 +4,29 @@
     {
       config,
       lib,
+      pkgs,
       ...
     }:
     let
       niri-command = "${lib.getExe' config.programs.niri.package "niri-session"}";
+      inherit (self.variables) username;
     in
     {
+      environment.systemPackages = [
+        pkgs.libsecret
+      ];
       services.displayManager.enable = lib.mkForce false;
       services.greetd = {
         enable = true;
         settings = {
           default_session = {
             command = niri-command;
-            user = self.variables.username;
+            user = username;
           };
         };
+      };
+      security.pam.services.greetd = {
+        enableGnomeKeyring = true;
       };
     };
 }
