3a3c0f9653195e180300f68a1121996824617777
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-18 22:00:02 +0000

    [bash] make the fish interactive part conditional if fish is enabled

--- modules/home/programs/shell/bash.nix
+++ modules/home/programs/shell/bash.nix
@@ -1,3 +1,4 @@
+{ self, ... }:
 {
   flake.modules.nixos.default =
     {
@@ -10,13 +11,15 @@
       programs.nano.enable = lib.mkForce false;
       programs.bash = {
         enable = true;
-        interactiveShellInit = ''
-          if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
-          then
-            shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
-            exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
-          fi
-        '';
+        interactiveShellInit =
+          lib.mkIf config.home-manager.users.${self.variables.username}.programs.fish.enable
+            ''
+              if [[ $(${pkgs.procps}/bin/ps --no-header --pid=$PPID --format=comm) != "fish" && -z ''${BASH_EXECUTION_STRING} ]]
+              then
+                shopt -q login_shell && LOGIN_OPTION='--login' || LOGIN_OPTION=""
+                exec ${pkgs.fish}/bin/fish $LOGIN_OPTION
+              fi
+            '';
         shellInit = ''
           export NOCTALIA_AP_GOOGLE_API_KEY="$(cat ${config.sops.secrets.gemini.path})"
         '';
