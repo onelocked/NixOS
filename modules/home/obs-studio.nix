d19f04bc6e1fee34ccad5e409a3c6d28f6789361
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-19 15:19:52 +0000

    [obs-studio] use the module that comes from derivations

--- modules/home/obs-studio.nix
+++ modules/home/obs-studio.nix
@@ -1,8 +1,11 @@
+{ inputs, ... }:
 {
+  flake-file.inputs.derivations.url = "git+file:///home/onelock/Development/Coding/derivations";
   flake.modules.homeManager = {
     obs-studio =
       { pkgs, ... }:
       {
+        imports = [ inputs.derivations.homeManagerModules.obs-with-plugins ];
         programs.obs-with-plugins = {
           enable = true;
           systemd = true;
@@ -10,74 +13,6 @@
             distroav
             obs-pipewire-audio-capture
           ];
-        };
-      };
-
-    default =
-      {
-        pkgs,
-        lib,
-        config,
-        ...
-      }:
-      let
-        cfg = config.programs.obs-with-plugins;
-
-        obs-wrapped = pkgs.wrapOBS {
-          plugins = cfg.plugins;
-        };
-
-        inherit (lib)
-          mkEnableOption
-          mkOption
-          mkIf
-          getExe
-          types
-          ;
-      in
-      {
-        options.programs.obs-with-plugins = {
-          enable = mkEnableOption "OBS Studio with configurable plugins";
-
-          plugins = mkOption {
-            type = types.listOf types.package;
-            default = [ ];
-            example = lib.literalExpression "with pkgs.obs-studio-plugins; [ distroav wlrobs ]";
-            description = "List of OBS plugins to include in the wrapper.";
-          };
-
-          systemd = mkOption {
-            type = types.bool;
-            default = false;
-            description = "Whether to enable the obs-startup systemd service.";
-          };
-        };
-
-        config = mkIf cfg.enable {
-          home.packages = [ obs-wrapped ];
-
-          systemd.user = mkIf cfg.systemd {
-            tmpfiles.rules = [
-              "R %h/.config/obs-studio/.sentinel"
-            ];
-
-            services."obs-startup" = {
-              Unit = {
-                Description = "OBS Auto Startup";
-                After = [ "quickshell.service" ];
-                Wants = [ "quickshell.service" ];
-              };
-
-              Service = {
-                Type = "simple";
-                ExecStart = getExe obs-wrapped;
-              };
-
-              Install = {
-                WantedBy = [ "quickshell.service" ];
-              };
-            };
-          };
         };
       };
   };
