5f7ddd26e0d945790603d6ce816195dcfe335792
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-19 04:37:24 +0000

    [vesktop] add declarative vesktop config

--- modules/home/programs/media/vesktop.nix
+++ modules/home/programs/media/vesktop.nix
@@ -1,9 +1,289 @@
 {
-  flake.modules.homeManager.vesktop =
-    { pkgs, ... }:
-    {
-      home.packages = with pkgs; [
-        vesktop
-      ];
+  flake.modules.homeManager.vesktop = {
+    programs.vesktop = {
+      enable = true;
+      settings = {
+        appBadge = false;
+        arRPC = true;
+        checkUpdates = false;
+        customTitleBar = false;
+        disableMinSize = false;
+        minimizeToTray = true;
+        tray = true;
+        splashTheming = true;
+        staticTitle = true;
+        hardwareAcceleration = true;
+        discordBranch = "stable";
+      };
+      vencord.settings = {
+        autoUpdate = true;
+        autoUpdateNotification = true;
+        useQuickCss = true;
+        themeLinks = [ "https://refact0r.github.io/system24/build/system24.css" ];
+        eagerPatches = false;
+        enabledThemes = [ ];
+        enableReactDevtools = false;
+        frameless = false;
+        transparent = false;
+        winCtrlQ = false;
+        disableMinSize = false;
+        winNativeTitleBar = false;
+
+        plugins = {
+          ChatInputButtonAPI.enabled = true;
+          CommandsAPI.enabled = true;
+          DynamicImageModalAPI.enabled = false;
+          MemberListDecoratorsAPI.enabled = true;
+          MessageAccessoriesAPI.enabled = true;
+          MessageDecorationsAPI.enabled = true;
+          MessageEventsAPI.enabled = false;
+          MessagePopoverAPI.enabled = false;
+          MessageUpdaterAPI.enabled = false;
+          ServerListAPI.enabled = false;
+          UserSettingsAPI.enabled = true;
+          AccountPanelServerProfile.enabled = false;
+          AlwaysAnimate.enabled = false;
+          AlwaysExpandRoles.enabled = false;
+          AlwaysTrust.enabled = false;
+          AnonymiseFileNames.enabled = false;
+          AppleMusicRichPresence.enabled = false;
+          "WebRichPresence (arRPC)".enabled = false;
+          BetterFolders.enabled = false;
+          BetterGifAltText.enabled = false;
+          BetterGifPicker.enabled = false;
+          BetterNotesBox.enabled = false;
+          BetterRoleContext.enabled = false;
+          BetterRoleDot = {
+            enabled = true;
+            bothStyles = false;
+            copyRoleColorInProfilePopout = false;
+          };
+          BetterSessions.enabled = false;
+          BetterSettings.enabled = false;
+          BetterUploadButton.enabled = false;
+          BiggerStreamPreview.enabled = false;
+          BlurNSFW.enabled = false;
+          CallTimer.enabled = false;
+          ClearURLs.enabled = false;
+          ClientTheme.enabled = false;
+          ColorSighted.enabled = false;
+          ConsoleJanitor.enabled = false;
+          ConsoleShortcuts.enabled = false;
+          CopyEmojiMarkdown.enabled = false;
+          CopyFileContents.enabled = false;
+          CopyStickerLinks.enabled = false;
+          CopyUserURLs.enabled = false;
+          CrashHandler.enabled = true;
+          CtrlEnterSend.enabled = false;
+          CustomCommands.enabled = false;
+          CustomIdle.enabled = false;
+          CustomRPC.enabled = false;
+          Dearrow.enabled = false;
+          Decor.enabled = false;
+          DisableCallIdle.enabled = false;
+          DontRoundMyTimestamps.enabled = false;
+          Experiments.enabled = false;
+          ExpressionCloner.enabled = false;
+          F8Break.enabled = false;
+          FakeNitro.enabled = false;
+          FakeProfileThemes.enabled = false;
+          FavoriteEmojiFirst.enabled = false;
+          FavoriteGifSearch.enabled = false;
+          FixCodeblockGap.enabled = true;
+          FixImagesQuality.enabled = false;
+          FixSpotifyEmbeds.enabled = false;
+          FixYoutubeEmbeds.enabled = false;
+          ForceOwnerCrown.enabled = true;
+          FriendInvites.enabled = false;
+          FriendsSince.enabled = false;
+          FullSearchContext.enabled = false;
+          FullUserInChatbox.enabled = false;
+          GameActivityToggle.enabled = false;
+          GifPaste.enabled = false;
+          GreetStickerPicker.enabled = false;
+          HideMedia.enabled = false;
+          iLoveSpam.enabled = false;
+          IgnoreActivities.enabled = false;
+          ImageFilename.enabled = false;
+          ImageLink.enabled = false;
+          ImageZoom = {
+            enabled = true;
+            saveZoomValues = true;
+            invertScroll = true;
+            nearestNeighbour = false;
+            square = false;
+            zoom = 5;
+            size = 550;
+            zoomSpeed = 0.5;
+          };
+          ImplicitRelationships.enabled = false;
+          IrcColors.enabled = false;
+          KeepCurrentChannel.enabled = false;
+          LastFMRichPresence.enabled = false;
+          LoadingQuotes.enabled = false;
+          MemberCount.enabled = false;
+          MentionAvatars.enabled = false;
+          MessageClickActions.enabled = false;
+          MessageLatency.enabled = false;
+          MessageLinkEmbeds.enabled = false;
+          MessageLogger.enabled = false;
+          MoreQuickReactions.enabled = false;
+          MutualGroupDMs.enabled = false;
+          NewGuildSettings.enabled = false;
+          NoBlockedMessages.enabled = false;
+          NoDefaultHangStatus.enabled = false;
+          NoDevtoolsWarning.enabled = false;
+          NoF1.enabled = false;
+          NoMaskedUrlPaste.enabled = false;
+          NoMosaic.enabled = false;
+          NoOnboardingDelay.enabled = false;
+          NoPendingCount.enabled = false;
+          NoProfileThemes.enabled = false;
+          NoReplyMention.enabled = false;
+          NoServerEmojis.enabled = false;
+          NoTypingAnimation.enabled = false;
+          NoUnblockToJump.enabled = false;
+          NotificationVolume.enabled = false;
+          OnePingPerDM.enabled = false;
+          oneko.enabled = false;
+          OpenInApp.enabled = false;
+          OverrideForumDefaults.enabled = false;
+          PauseInvitesForever.enabled = false;
+          PermissionFreeWill.enabled = false;
+          PermissionsViewer.enabled = false;
+          petpet.enabled = false;
+          PictureInPicture.enabled = false;
+          PinDMs = {
+            enabled = true;
+            userBasedCategoryList = {
+              "613524063809437736" = [ ];
+            };
+            canCollapseDmSection = false;
+            pinOrder = 0;
+          };
+          PlainFolderIcon.enabled = false;
+          PlatformIndicators.enabled = false;
+          PreviewMessage.enabled = false;
+          QuickMention.enabled = false;
+          QuickReply.enabled = false;
+          ReactErrorDecoder.enabled = false;
+          ReadAllNotificationsButton.enabled = false;
+          RelationshipNotifier.enabled = false;
+          ReplaceGoogleSearch.enabled = false;
+          ReplyTimestamp.enabled = false;
+          RevealAllSpoilers.enabled = false;
+          ReverseImageSearch.enabled = false;
+          ReviewDB.enabled = false;
+          RoleColorEverywhere = {
+            enabled = true;
+            chatMentions = true;
+            memberList = true;
+            voiceUsers = true;
+            reactorsList = true;
+            pollResults = true;
+            colorChatMessages = false;
+          };
+          SecretRingToneEnabler.enabled = false;
+          Summaries.enabled = false;
+          SendTimestamps.enabled = false;
+          ServerInfo.enabled = false;
+          ServerListIndicators.enabled = false;
+          ShikiCodeblocks.enabled = false;
+          ShowAllMessageButtons.enabled = false;
+          ShowConnections.enabled = false;
+          ShowHiddenChannels.enabled = false;
+          ShowHiddenThings.enabled = false;
+          ShowMeYourName.enabled = false;
+          ShowTimeoutDuration.enabled = false;
+          SilentMessageToggle.enabled = false;
+          SilentTyping = {
+            enabled = true;
+            isEnabled = true;
+            showIcon = false;
+          };
+          SortFriendRequests.enabled = false;
+          SpotifyControls.enabled = false;
+          SpotifyCrack.enabled = false;
+          SpotifyShareCommands.enabled = false;
+          StartupTimings.enabled = false;
+          StickerPaste.enabled = false;
+          StreamerModeOnStream.enabled = false;
+          SuperReactionTweaks.enabled = false;
+          TextReplace.enabled = false;
+          ThemeAttributes.enabled = false;
+          Translate.enabled = false;
+          TypingIndicator = {
+            enabled = true;
+            includeMutedChannels = false;
+            includeCurrentChannel = true;
+            indicatorMode = 3;
+          };
+          TypingTweaks = {
+            enabled = true;
+            alternativeFormatting = true;
+            showRoleColors = true;
+            showAvatars = true;
+          };
+          Unindent.enabled = false;
+          UnlockedAvatarZoom.enabled = false;
+          UnsuppressEmbeds.enabled = false;
+          UserMessagesPronouns.enabled = false;
+          UserVoiceShow = {
+            enabled = true;
+            showInUserProfileModal = true;
+            showInMemberList = true;
+            showInMessages = true;
+          };
+          USRBG.enabled = false;
+          ValidReply.enabled = false;
+          ValidUser.enabled = false;
+          VoiceChatDoubleClick.enabled = true;
+          VcNarrator.enabled = false;
+          VencordToolbox.enabled = false;
+          ViewIcons.enabled = false;
+          ViewRaw.enabled = false;
+          VoiceDownload.enabled = false;
+          VoiceMessages.enabled = false;
+          VolumeBooster.enabled = false;
+          WebKeybinds.enabled = true;
+          WebScreenShareFixes.enabled = true;
+          WhoReacted.enabled = false;
+          XSOverlay.enabled = false;
+          YoutubeAdblock.enabled = false;
+          BadgeAPI.enabled = true;
+          NoTrack = {
+            enabled = true;
+            disableAnalytics = true;
+          };
+          Settings = {
+            enabled = true;
+            settingsLocation = "aboveNitro";
+          };
+          DisableDeepLinks.enabled = true;
+          SupportHelper.enabled = true;
+          WebContextMenus.enabled = true;
+        };
+
+        uiElements = {
+          chatBarButtons = { };
+          messagePopoverButtons = { };
+        };
+
+        notifications = {
+          timeout = 5000;
+          position = "bottom-right";
+          useNative = "not-focused";
+          logLimit = 50;
+        };
+
+        cloud = {
+          authenticated = false;
+          url = "";
+          settingsSync = false;
+          settingsSyncVersion = 1773894758681;
+        };
+      };
+
     };
+  };
 }
