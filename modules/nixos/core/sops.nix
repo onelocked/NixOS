a76d26bc8285b39c1a27bf45e483a72f959bdd32
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-19 03:40:25 +0000

    [sops] config cleanup

--- modules/nixos/core/sops.nix
+++ modules/nixos/core/sops.nix
@@ -13,16 +13,16 @@
     { pkgs, config, ... }:
     {
       imports = [ inputs.sops-nix.nixosModules.sops ];
-      environment.systemPackages = with pkgs; [
-        sops
-      ];
-      sops.defaultSopsFile = ../../../secrets/gemini.yaml;
-      sops.age.keyFile = self.variables.homedir + "/.config/sops/age/keys.txt";
-      sops.age.generateKey = true;
-      sops.secrets.gemini = {
-        owner = "onelock";
-        format = "yaml";
-        sopsFile = config.sops.defaultSopsFile;
+      environment.systemPackages = with pkgs; [ sops ];
+      sops = {
+        defaultSopsFile = ../../../secrets/gemini.yaml;
+        age.keyFile = self.variables.homedir + "/.config/sops/age/keys.txt";
+        age.generateKey = true;
+        secrets.gemini = {
+          owner = "onelock";
+          format = "yaml";
+          sopsFile = config.sops.defaultSopsFile;
+        };
       };
     };
 }
