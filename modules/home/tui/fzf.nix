aaced8fd2a06b240eb393eb02dc8041e01456d57
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-22 01:31:20 +0000

    [fzf] add fuzzy finder config

---
+++ modules/home/tui/fzf.nix
@@ -0,0 +1,32 @@
+{
+  flake.modules.homeManager.default = {
+    programs.fzf = {
+      enable = true;
+      enableFishIntegration = true;
+      colors = {
+        "bg+" = "#2a2a3a";
+        "fg+" = "#cfd3e7";
+        "hl" = "#b8db8c";
+        "hl+" = "#b8db8c";
+        "info" = "#a8c8f0";
+        "prompt" = "#c8b0e8";
+        "pointer" = "#e8c4d8";
+        "marker" = "#8fd4b5";
+        "spinner" = "#7cb8d4";
+        "header" = "#7d75c0";
+        "border" = "#454545";
+        "gutter" = "#2a2a3a";
+      };
+      defaultOptions = [
+        "--height=50%"
+        "--layout=reverse"
+        "--border=rounded"
+        "--prompt= "
+        "--pointer=➜ "
+        "--marker= "
+        "--info=inline"
+        "--no-clear"
+      ];
+    };
+  };
+}
