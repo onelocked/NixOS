11502de56aa0a2db69081e24bba0d4e1bb8e8127
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-22 17:50:00 +0000

    [yazi] preview certain directories with eza

--- modules/home/tui/yazi/yazi-plugins.nix
+++ modules/home/tui/yazi/yazi-plugins.nix
@@ -1,3 +1,4 @@
+{ self, ... }:
 {
   flake.modules.homeManager.yazi =
     {
@@ -7,69 +8,81 @@
       ...
     }:
     {
-      programs.yazi = {
-        plugins =
-          let
-            pluginNames = [
-              "starship"
-              "full-border"
-              "no-status"
-              "ouch"
-              "lazygit"
-              "git"
-              "piper"
-            ];
-          in
-          builtins.listToAttrs (
-            map (name: {
-              inherit name;
-              value = pkgs.yaziPlugins.${name};
-            }) pluginNames
-          );
-        settings = {
-          plugin = {
-            prepend_previewers = [
-              {
-                url = "*.md";
-                run = "piper -- CLICOLOR_FORCE=1 ${lib.getExe pkgs.glow} -w=$w -s=dracula -- $1";
-              }
-            ];
-            prepend_preloaders = [
-              {
-                url = "*.md";
-                run = "piper -- CLICOLOR_FORCE=1 ${lib.getExe pkgs.glow} -w=$w -s=dracula -- $1";
-              }
-            ];
-            prepend_fetchers = [
-              {
-                id = "git";
-                url = "*";
-                run = "git";
-              }
-              {
-                id = "git";
-                url = "*/";
-                run = "git";
-              }
-            ];
+      programs.yazi =
+        let
+          _ = lib.getExe;
+        in
+        {
+          plugins =
+            let
+              pluginNames = [
+                "starship"
+                "full-border"
+                "no-status"
+                "ouch"
+                "lazygit"
+                "git"
+                "piper"
+              ];
+            in
+            builtins.listToAttrs (
+              map (name: {
+                inherit name;
+                value = pkgs.yaziPlugins.${name};
+              }) pluginNames
+            );
+          settings = {
+            plugin = {
+              prepend_previewers = [
+                {
+                  url = "*.md";
+                  run = "piper -- CLICOLOR_FORCE=1 ${_ pkgs.glow} -w=$w -s=dracula -- $1";
+                }
+                {
+                  url = self.variables.homedir + "/NixOS" + "/**/";
+                  run = "piper -- ${_ pkgs.eza} -TL=3 --color=always --icons=always --group-directories-first --no-quotes $1";
+                }
+                {
+                  url = config.xdg.userDirs.extraConfig.PROJECTS + "/**/";
+                  run = "piper -- ${_ pkgs.eza} -TL=3 --color=always --icons=always --group-directories-first --no-quotes $1";
+                }
+              ];
+              prepend_preloaders = [
+                {
+                  url = "*.md";
+                  run = "piper -- CLICOLOR_FORCE=1 ${_ pkgs.glow} -w=$w -s=dracula -- $1";
+                }
+              ];
+              prepend_fetchers = [
+                {
+                  id = "git";
+                  url = "*";
+                  run = "git";
+                }
+                {
+                  id = "git";
+                  url = "*/";
+                  run = "git";
+                }
+              ];
+            };
           };
+          initLua = # lua
+            ''
+              require("starship"):setup({
+                  hide_flags = false, -- Default: false
+                  flags_after_prompt = true, -- Default: true
+                  config_file = "${config.xdg.configHome}/starship.toml", -- Default: nil
+              })
+              require("no-status"):setup()
+              require("full-border"):setup {
+              	type = ui.Border.ROUNDED,
+              }
+              require("git"):setup {
+                  -- Order of status signs showing in the linemode
+                order = 1500,
+              }
+            '';
         };
-        initLua = # lua
-          ''
-            require("starship"):setup({
-                hide_flags = false, -- Default: false
-                flags_after_prompt = true, -- Default: true
-                config_file = "${config.xdg.configHome}/starship.toml", -- Default: nil
-            })
-            require("no-status"):setup()
-            require("full-border"):setup {
-            	type = ui.Border.ROUNDED,
-            }
-            require("git"):setup {
-                -- Order of status signs showing in the linemode
-              order = 1500,
-            }
-          '';
-      };
     };
 }
