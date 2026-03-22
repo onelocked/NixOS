49b014e79bf592752fdab81421c1fcbec58c898b
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-22 01:58:06 +0000

    [tmux] zellij like keybinds

--- modules/home/tui/tmux.nix
+++ modules/home/tui/tmux.nix
@@ -2,17 +2,6 @@
   flake.modules.homeManager.tmux =
     { pkgs, ... }:
     let
-      catppuccin-tmux = pkgs.tmuxPlugins.mkTmuxPlugin {
-        pluginName = "catppuccin-tmux";
-        version = "unstable-30-11-23";
-        rtpFilePath = "catppuccin.tmux";
-        src = pkgs.fetchFromGitHub {
-          owner = "omerxx";
-          repo = "catppuccin-tmux";
-          rev = "e30336b79986e87b1f99e6bd9ec83cffd1da2017";
-          hash = "sha256-Ig6+pB8us6YSMHwSRU3sLr9sK+L7kbx2kgxzgmpR920=";
-        };
-      };
       tmux-floax = pkgs.tmuxPlugins.mkTmuxPlugin {
         pluginName = "tmux-floax";
         version = "unstable-21-11-24";
@@ -45,92 +34,96 @@
           ''
             unbind '['
 
-
+            #enter vim style copy mode
             bind Space copy-mode
+            #create a new tab
             bind ^C new-window -c "$HOME"
             bind ^D detach
             bind * list-clients
-            bind H previous-window
-            bind L next-window
-            bind r command-prompt "rename-window %%"
-            bind ^A last-window
-            bind ^W list-windows
-            bind w list-windows
-            bind z resize-pane -Z
-            bind ^L refresh-client
-            bind l refresh-client
-            bind | split-window
-            bind s split-window -v -c "#{pane_current_path}"
-            bind v split-window -h -c "#{pane_current_path}"
-            bind h select-pane -L
-            bind j select-pane -D
-            bind k select-pane -U
-            bind l select-pane -R
-            bind -r -T prefix , resize-pane -L 20
-            bind -r -T prefix . resize-pane -R 20
-            bind -r -T prefix - resize-pane -D 7
-            bind -r -T prefix = resize-pane -U 7
-            bind : command-prompt
             bind * setw synchronize-panes
-            bind P set pane-border-status
-            bind -n C-M-c kill-pane
-            bind c new-window -c "#{pane_current_path}"
             bind x swap-pane -D
             bind O choose-session
             bind K send-keys "clear"\; send-keys "Enter"
 
+            # fullscreen a pane
+            bind -n M-f resize-pane -Z
+
+            # kill pane with Ctrl + X
+            bind -n C-x kill-pane
+
+
+            # Pane movement
+            bind -n M-Left  select-pane -L
+            bind -n M-Down  select-pane -D
+            bind -n M-Up    select-pane -U
+            bind -n M-Right select-pane -R
+
+
+            # create tabs
+            bind-key t switch-client -T tab_table
+            bind-key -T tab_table n new-window -c "#{pane_current_path}"
+            bind c new-window -c "#{pane_current_path}"
+            # creating pane splits
+            bind-key p switch-client -T pane_table
+            bind-key -T pane_table n split-window -h -c "#{pane_current_path}"
+            bind-key -T pane_table d split-window -v -c "#{pane_current_path}"
+            bind -n C-n split-window -h -c "#{pane_current_path}"
+
+            bind-key r switch-client -T resize_table
+            bind-key -T resize_table Left  resize-pane -L 5 \; switch-client -T resize_table
+            bind-key -T resize_table Right resize-pane -R 5 \; switch-client -T resize_table
+            bind-key -T resize_table Up    resize-pane -U 5 \; switch-client -T resize_table
+            bind-key -T resize_table Down  resize-pane -D 5 \; switch-client -T resize_table
+            bind-key -T resize_table Escape switch-client -T root
+
+
+            set -g prefix ^A #Leader key
             set -g allow-passthrough on
             set -ga update-environment TERM
             set -ga update-environment TERM_PROGRAM
             set -g renumber-windows on
             set -g base-index 1
             setw -g pane-base-index 1
-            set -g prefix ^A
             set -g detach-on-destroy off
             set -g escape-time 0
             set -g set-clipboard on
-            set -g status-position top
 
-            set-option -g default-terminal 'screen-256color'
-            set-option -g terminal-overrides ',xterm-256color:RGB'
+            set-option -g default-terminal 'tmux-256color'
+            set-option -ga terminal-features 'tmux-256color:sixel'
+            set-option -g terminal-overrides ',foot*:Tc'
 
-            set -g @catppuccin_window_left_separator ""
-            set -g @catppuccin_window_right_separator " "
-            set -g @catppuccin_window_middle_separator " █"
-            set -g @catppuccin_window_number_position "right"
-            set -g @catppuccin_window_default_fill "number"
-            set -g @catppuccin_window_default_text "#W"
-            set -g @catppuccin_window_current_fill "number"
-            set -g @catppuccin_window_current_text "#W#{?window_zoomed_flag,(),}"
-            set -g @catppuccin_status_modules_right "directory date_time"
-            set -g @catppuccin_status_modules_left "session"
-            set -g @catppuccin_status_left_separator " "
-            set -g @catppuccin_status_right_separator " "
-            set -g @catppuccin_status_right_separator_inverse "no"
-            set -g @catppuccin_status_fill "icon"
-            set -g @catppuccin_status_connect_separator "no"
-            set -g @catppuccin_directory_text "#{b:pane_current_path}"
-            set -g @catppuccin_date_time_text "%H:%M"
-            ${tmuxRun catppuccin-tmux}
+            # pane border color
+            set -g pane-border-style "fg=#2a2a2a"
+            set -g pane-active-border-style "fg=#2a2a2a"
 
+            #border style
+            set -g pane-border-style "fg=#2a2a2a"
+            set -g pane-active-border-style "fg=#c5c0ff"
 
+            set -g status-position bottom
+            set -g status-style "bg=default,fg=#8fd4b5"
+            set -g status-left "#{?client_prefix,  ,  }"
+            set -g status-right ""
+            set -g status-justify centre
 
-            set -g @fzf-url-fzf-options '-p 60%,30% --prompt="    " --border-label=" Open URL "'
-            set -g @fzf-url-history-limit '2000'
-            ${tmuxRun pkgs.tmuxPlugins.tmux-fzf}
-            ${tmuxRun pkgs.tmuxPlugins.fzf-tmux-url}
+            set -g window-status-format "#[fg=colour238]● "
+            set -g window-status-current-format "#{?window_zoomed_flag,#[fg=#c5c0ff]●  ,#[fg=#c5c0ff]● }"
+            set -g window-status-separator "  "
 
+            #Floax plugin
             set -g @floax-width '80%'
             set -g @floax-height '80%'
             set -g @floax-border-color 'white'
             set -g @floax-text-color 'blue'
-            set -g @floax-bind 'e'
+            bind -n C-f run-shell "${tmux-floax}/share/tmux-plugins/tmux-floax/scripts/floax.sh"
             set -g @floax-change-path 'true'
             ${tmuxRun tmux-floax}
 
             set -g @sessionx-bind-zo-new-window 'ctrl-y'
             set -g @sessionx-auto-accept 'off'
-            set -g @sessionx-bind 'S'
+            bind -n C-f run-shell "${tmux-floax}/share/tmux-plugins/tmux-floax/scripts/floax.sh"
+            unbind -T prefix p
+            bind-key p switch-client -T pane_table
             set -g @sessionx-window-height '86%'
             set -g @sessionx-window-width '75%'
             set -g @sessionx-zoxide-mode 'on'
