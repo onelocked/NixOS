eaaa958b42a6244c4cad4208b0a0961223980930
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-28 03:51:26 +0000

    [btop] config improvements

--- modules/home/cli/btop.nix
+++ modules/home/cli/btop.nix
@@ -4,9 +4,15 @@
     {
       programs.btop = {
         enable = true;
-        package = pkgs.btop-rocm;
+        package = pkgs.btop;
         settings = {
           color_theme = "noctalia";
+          vim_keys = true;
+          rounded_corners = true;
+          proc_tree = false;
+          show_gpu_info = "on";
+          show_uptime = true;
+          show_coretemp = true;
         };
       };
     };
