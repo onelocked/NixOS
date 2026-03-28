ee01784af5ebb8495a677d03a960604814683716
onelocked <180497738+onelocked@users.noreply.github.com>
2026-03-28 17:30:49 +0000

    [yazi] copy to clipboard Ctrl + y keybind

--- modules/home/tui/yazi/yazi-keybinds.nix
+++ modules/home/tui/yazi/yazi-keybinds.nix
@@ -1,44 +1,45 @@
 {
-  flake.modules.homeManager.yazi = {
-    programs.yazi = {
-      keymap = {
-        mgr.prepend_keymap = [
-          {
-            on = [
-              "l"
-              "g"
-            ];
-            run = "plugin lazygit";
-            desc = "run lazygit";
-          }
-          {
-            on = [
-              "g"
-              "r"
-            ];
-            run = ''shell -- ya emit cd "$(git rev-parse --show-toplevel)"'';
-            desc = "git root";
-          }
-          {
-            on = [
-              "g"
-              "s"
-            ];
-            run = "shell -- ya emit cd /mnt/s3";
-            desc = "Go to S3";
-          }
-          {
-            on = [
-              "b"
-              "y"
-            ];
-            run = [
-              ''shell -- for path in %s; do echo "file://$path"; done | wl-copy -t text/uri-list''
-            ];
-            desc = "copy to clipboard";
-          }
-        ];
+  flake.modules.homeManager.yazi =
+    { pkgs, ... }:
+    {
+      programs.yazi = {
+        keymap = {
+          mgr.prepend_keymap = [
+            {
+              on = [
+                "l"
+                "g"
+              ];
+              run = "plugin lazygit";
+              desc = "run lazygit";
+            }
+            {
+              on = [
+                "g"
+                "r"
+              ];
+              run = ''shell -- ya emit cd "$(git rev-parse --show-toplevel)"'';
+              desc = "git root";
+            }
+            {
+              on = [
+                "g"
+                "s"
+              ];
+              run = "shell -- ya emit cd /mnt/s3";
+              desc = "Go to S3";
+            }
+            {
+              on = [
+                "<C-y>"
+              ];
+              run = [
+                ''shell -- for path in %s; do echo "file://$path"; done | ${pkgs.wl-clipboard-rs}/bin/wl-copy -t text/uri-list''
+              ];
+              desc = "copy to clipboard";
+            }
+          ];
+        };
       };
     };
-  };
 }
