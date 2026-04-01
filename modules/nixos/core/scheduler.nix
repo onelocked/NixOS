848cfc16e1a282ac41dd64c121ac4c7d1d01a71f
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-01 01:13:23 +0100

    [system76] scheduler

--- modules/nixos/core/scheduler.nix
+++ modules/nixos/core/scheduler.nix
@@ -3,10 +3,14 @@
     { pkgs, ... }:
     {
       services = {
+        system76-scheduler = {
+          enable = true;
+          useStockConfig = true;
+        };
         scx = {
           enable = true;
           package = pkgs.scx.rustscheds;
-          scheduler = "scx_rusty"; # https://github.com/sched-ext/scx/blob/main/scheds/rust/README.md
+          scheduler = "scx_lavd"; # https://github.com/sched-ext/scx/blob/main/scheds/rust/README.md
         };
       };
     };
