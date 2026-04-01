87e22de7225c856cc9a8ed25d0c3abbc6631edd0
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-01 16:48:07 +0100

    [worktrunk] remove flake input, the package is now available in nixpkgs

--- modules/home/cli/worktrunk.nix
+++ modules/home/cli/worktrunk.nix
@@ -1,15 +1,12 @@
-{ inputs, ... }:
 {
-  flake-file.inputs.worktrunk = {
-    url = "github:max-sixty/worktrunk/fe395cb467d341d4b21e9b1cc6cfa2feaec0fdd1";
-    inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  flake.modules.homeManager.worktrunk = {
-    imports = [ inputs.worktrunk.homeModules.default ];
-    programs.worktrunk = {
-      enable = true;
-      enableFishIntegration = true;
+  flake.modules.homeManager.worktrunk =
+    { pkgs, lib, ... }:
+    {
+      home.packages = [ pkgs.worktrunk ];
+      programs = {
+        fish.interactiveShellInit = ''
+          ${lib.getExe pkgs.worktrunk} config shell init fish | source
+        '';
+      };
     };
-  };
 }
