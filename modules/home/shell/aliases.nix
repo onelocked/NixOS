ca139372046a3d1b3c4f1eb714ba56f437a0807f
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-19 01:54:07 +0100

    atuin: wrapped

--- modules/home/shell/aliases.nix
+++ modules/home/shell/aliases.nix
@@ -1,3 +1,4 @@
+{ inputs, ... }:
 {
   m.default =
     {
@@ -5,6 +6,23 @@
       lib,
       ...
     }:
+    let
+      tomlFormat = pkgs.formats.toml { };
+      atuin = inputs.wrappers.lib.wrapPackage {
+        inherit pkgs;
+        package = pkgs.atuin;
+        env.ATUIN_CONFIG_DIR = pkgs.linkFarm "atuin-config" [
+          {
+            name = "config.toml";
+            path = tomlFormat.generate "config.toml" {
+              enter_accept = true;
+              filter_mode = "session-preload";
+              search_mode = "fuzzy";
+            };
+          }
+        ];
+      };
+    in
     {
       environment.shellAliases =
         let
@@ -20,15 +38,14 @@
           gr = "cd (git rev-parse --show-toplevel)";
           shot = "${wl-clipboard-rs}/bin/wl-paste | , silicon --language nix --to-clipboard --shadow-blur-radius 30 --pad-horiz 20 --pad-vert 20 --theme 'Visual Studio Dark+'";
         };
-
-      hj.packages = [ pkgs.atuin ];
+      hj.packages = [ atuin ];
       programs = {
         pay-respects.enable = true;
         fish.interactiveShellInit = ''
           ${lib.getExe pkgs.pay-respects} fish | source
           ${lib.getExe pkgs.nix-your-shell} fish | source
           ${lib.getExe pkgs.carapace} _carapace fish | source
-
+          ${lib.getExe atuin} init fish | source
         '';
         zoxide = {
           enable = true;
