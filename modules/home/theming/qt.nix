c5257f7474c9032fbfeb60918b74d5b9bf06f2f0
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-22 12:24:31 +0100

    qt: config improvement/cleanup

--- modules/home/theming/qt.nix
+++ modules/home/theming/qt.nix
@@ -3,50 +3,52 @@
   m.qt =
     { config, pkgs, ... }:
     let
+      iniFmt = (pkgs.formats.ini { }).generate;
+
       qtctConf = {
         Appearance = {
-          color_scheme_path = config.hj.xdg.config.directory + "/qt6ct/colors/noctalia.conf";
+          color_scheme_path = colorSchemeFile;
           custom_palette = true;
           icon_theme = config.custom.gtk.iconTheme.name;
           standard_dialogs = "default";
-          style = "Fusion";
+          style = "Adwaita-Dark";
+        };
+        Fonts = {
+          fixed = font;
+          general = font;
         };
       };
-      defaultFont = "${config.custom.gtk.font.name},12";
+
+      font = ''"${config.custom.gtk.font.name},12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'';
+
+      qtctFile.source = iniFmt "qtct.conf" qtctConf;
+
+      palette = "#e5e1e6, #131316, #ffffff, #cacaca, #9f9f9f, #b8b8b8, #e5e1e6, #ffffff, #e5e1e6, #131316, #131316, #000000, #413b8e, #e3dfff, #c7c4dc, #c5c0ff, #47464f, #131316, #47464f, #e5e1e6, #e5e1e6, #c5c0ff";
+      colorSchemeFile = iniFmt "colors.conf" {
+        ColorScheme =
+          [
+            "active_colors"
+            "disabled_colors"
+            "inactive_colors"
+          ]
+          |> (names: lib.genAttrs names (_: palette));
+      };
     in
     {
-      environment = {
-        systemPackages = with pkgs.qt6Packages; [
-          qt6ct
-          qtwayland
-        ];
-      };
+      environment.systemPackages = with pkgs.qt6Packages; [
+        qt6ct
+        qtwayland
+      ];
 
-      # use gtk theme on qt apps
       qt = {
         enable = true;
         platformTheme = "qt5ct";
         style = "adwaita-dark";
       };
 
-      # use dynamic theme for qt5ct.conf and qt6ct.conf
-      hj.xdg.config.files =
-        let
-          font = ''"${defaultFont},-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'';
-          conf = qtctConf // {
-            Fonts = {
-              fixed = font;
-              general = font;
-            };
-          };
-        in
-        [
-          "qt5ct"
-          "qt6ct"
-        ]
-        |> map (
-          version: lib.nameValuePair "${version}/${version}.conf" { text = lib.generators.toINI { } conf; }
-        )
-        |> builtins.listToAttrs;
+      hj.xdg.config.files = {
+        "qt5ct/qt5ct.conf" = qtctFile;
+        "qt6ct/qt6ct.conf" = qtctFile;
+      };
     };
 }
