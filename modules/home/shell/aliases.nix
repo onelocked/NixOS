84a62e4ce92e077045b6acbfc20d9bc30891477a
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-22 23:56:49 +0100

    fish: aliases cleanup

--- modules/home/shell/aliases.nix
+++ modules/home/shell/aliases.nix
@@ -7,6 +7,7 @@
       ...
     }:
     let
+      _ = lib.getExe;
       tomlFormat = pkgs.formats.toml { };
       atuin = inputs.wrappers.lib.wrapPackage {
         inherit pkgs;
@@ -24,27 +25,22 @@
       };
     in
     {
-      environment.shellAliases =
-        let
-          _ = lib.getExe;
-        in
-        with pkgs;
-        {
-          ping = "${_ gping}";
-          cat = "${_ bat}";
-          zip = "${_ zip}";
-          gtop = "${_ amdgpu_top} --dark";
-          gr = "cd (git rev-parse --show-toplevel)";
-          shot = "${wl-clipboard-rs}/bin/wl-paste | , silicon --language nix --to-clipboard --shadow-blur-radius 30 --pad-horiz 20 --pad-vert 20 --theme 'Visual Studio Dark+'";
-        };
+      environment.shellAliases = with pkgs; {
+        ping = "${_ gping}";
+        cat = "${_ bat}";
+        zip = "${_ zip}";
+        gtop = "${_ amdgpu_top} --dark";
+        gr = "cd (git rev-parse --show-toplevel)";
+        shot = "${wl-clipboard-rs}/bin/wl-paste | , silicon --language nix --to-clipboard --shadow-blur-radius 30 --pad-horiz 20 --pad-vert 20 --theme 'Visual Studio Dark+'";
+      };
       hj.packages = [ atuin ];
       programs = {
         pay-respects.enable = true;
         fish.interactiveShellInit = ''
-          ${lib.getExe pkgs.pay-respects} fish | source
-          ${lib.getExe pkgs.nix-your-shell} fish | source
-          ${lib.getExe pkgs.carapace} _carapace fish | source
-          ${lib.getExe atuin} init fish | source
+          ${_ pkgs.pay-respects} fish | source
+          ${_ pkgs.nix-your-shell} fish | source
+          ${_ pkgs.carapace} _carapace fish | source
+          ${_ atuin} init fish | source
         '';
         zoxide = {
           enable = true;
