b84ca15721efe648829ab9a29398e225440ddca0
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-25 21:25:57 +0100

    nix: flake update inputs

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     };
 
   inputs = {
-    nixpkgs.follows = "extra-modules/nixpkgs";
+    nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
     flake-parts = {
       url = "github:hercules-ci/flake-parts";
       inputs.nixpkgs-lib.follows = "nixpkgs";
@@ -21,14 +21,6 @@
     wrappers = {
       url = "github:BirdeeHub/nix-wrapper-modules";
       inputs.nixpkgs.follows = "nixpkgs";
-    };
-    extra-modules = {
-      url = "github:onelocked/extra-modules";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        import-tree.follows = "import-tree";
-        systems.follows = "systems";
-      };
     };
     flake-compat.url = "github:NixOS/flake-compat";
     fuzzy-search-yazi = {
@@ -43,6 +35,20 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     import-tree.url = "github:vic/import-tree";
+    lan-mouse = {
+      url = "github:feschber/lan-mouse";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
+    niri = {
+      url = "github:niri-wm/niri/b82d52705e1424cf47b26dd7b096832901c31f56";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        rust-overlay.follows = "rust-overlay";
+      };
+    };
     nix-cachyos-kernel = {
       url = "github:xddxdd/nix-cachyos-kernel/release";
       inputs = {
@@ -52,6 +58,23 @@
     };
     nix-index-database = {
       url = "github:nix-community/nix-index-database";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    qml-niri = {
+      url = "github:imiric/qml-niri/main";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        quickshell.follows = "quickshell";
+        treefmt-nix.follows = "treefmt-nix";
+      };
+    };
+    quickshell = {
+      url = "github:quickshell-mirror/quickshell";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {
@@ -66,13 +89,24 @@
       };
     };
     systems.url = "github:nix-systems/x86_64-linux";
+    treefmt-nix = {
+      url = "github:numtide/treefmt-nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    vicinae = {
+      url = "github:vicinaehq/vicinae";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        systems.follows = "systems";
+      };
+    };
     vicinae-extensions = {
       url = "github:vicinaehq/extensions";
       inputs = {
         nixpkgs.follows = "nixpkgs";
         flake-compat.follows = "flake-compat";
         systems.follows = "systems";
-        vicinae.follows = "extra-modules/vicinae";
+        vicinae.follows = "vicinae";
       };
     };
     vimmax = {
