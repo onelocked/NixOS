d0875c17d74fece16957272c791906b096105b05
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-25 19:43:05 +0100

    amd_gpu top

--- modules/hosts/mini-pc/hardware/amd-gpu.nix
+++ modules/hosts/mini-pc/hardware/amd-gpu.nix
@@ -1,3 +1,4 @@
+{ self, ... }:
 {
   m.hardware-mini-pc =
     { pkgs, ... }:
@@ -7,5 +8,22 @@
       hardware.amdgpu = {
         opencl.enable = true;
       };
+      nixpkgs.overlays = [
+        (final: _: {
+          amdgpu_top = self.packages.${final.stdenv.hostPlatform.system}.amdgpu_top;
+        })
+      ];
+    };
+  perSystem =
+    { pkgs, ... }:
+    {
+      packages.amdgpu_top = pkgs.amdgpu_top.overrideAttrs (oldAttrs: {
+        doCheck = false;
+        cargoBuildFlags = (oldAttrs.cargoBuildFlags or [ ]) ++ [
+          "--no-default-features"
+          "--features"
+          "tui,libamdgpu_top/libdrm_link"
+        ];
+      });
     };
 }
