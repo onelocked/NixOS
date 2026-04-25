ae53fdae853567d0b6e436c998ab083ccf688e62
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-25 20:36:28 +0100

    fonts: apple-font

--- modules/nixos/desktop/fonts.nix
+++ modules/nixos/desktop/fonts.nix
@@ -1,6 +1,22 @@
 {
   m.desktop =
     { pkgs, ... }:
+    let
+      apple-font = pkgs.stdenvNoCC.mkDerivation (finalAttrs: {
+        name = "apple-font";
+        src = fetchTarball {
+          url = "https://s3.onelock.org/download/fonts/apple-nerd.tar.gz";
+          sha256 = "0ra0rv5cfw6gcdqam370g69afybbagqfyg03lf1l05m2bs3cpr29";
+        };
+
+        nativeBuildInputs = [ pkgs.rsync ];
+
+        installPhase = ''
+          mkdir -p $out/share/fonts/apple-nerd
+          rsync -r --exclude='NY/*Black*' --exclude='NY/*Heavy*' ${finalAttrs.src}/ $out/share/fonts/apple-nerd
+        '';
+      });
+    in
     {
       fonts = {
         packages = with pkgs; [
