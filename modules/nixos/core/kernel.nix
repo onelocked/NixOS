6b88bb4cc47bcb2e03d482bbabfb4e4e3d3145cf
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-26 04:03:47 +0100

    kernel: add back the attic substituter

--- modules/nixos/core/kernel.nix
+++ modules/nixos/core/kernel.nix
@@ -12,13 +12,17 @@
     { pkgs, ... }:
     {
       nix.settings = {
-        substituters = [ "https://cache.garnix.io" ];
-        trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+        substituters = [
+          "https://cache.garnix.io"
+          "https://attic.xuyh0120.win/lantian"
+        ];
+        trusted-public-keys = [
+          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+          "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
+        ];
       };
-      nixpkgs.overlays = [
-        # Guarantees you have binary cache, but initializes another nixpkgs instance.
-        inputs.nix-cachyos-kernel.overlays.pinned
-      ];
+      nixpkgs.overlays = [ inputs.nix-cachyos-kernel.overlays.pinned ]; # Guarantees you have binary cache, but initializes another nixpkgs instance.
+
       # boot.kernelPackages = pkgs.linuxPackages_latest; # _latest, _zen, _xanmod_latest, _hardened, _rt
       boot.kernelPackages = pkgs.cachyosKernels.linuxPackages-cachyos-latest;
     };
