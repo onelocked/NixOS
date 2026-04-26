9b8bc6b8c554677d2306988ea6150d09fe43c860
onelocked <180497738+onelocked@users.noreply.github.com>
2026-04-26 16:44:04 +0100

    worktrunk: wrapped

--- modules/home/cli/worktrunk.nix
+++ modules/home/cli/worktrunk.nix
@@ -1,12 +1,32 @@
+{ inputs, ... }:
 {
   m.worktrunk =
     { pkgs, lib, ... }:
+    let
+      tomlFormat = pkgs.formats.toml { };
+    in
     {
+      programs.fish.interactiveShellInit = "${lib.getExe pkgs.worktrunk} config shell init fish | source ";
       hj.packages = [ pkgs.worktrunk ];
-      programs = {
-        fish.interactiveShellInit = ''
-          ${lib.getExe pkgs.worktrunk} config shell init fish | source
-        '';
-      };
+      nixpkgs.overlays = [
+        (_: prev: {
+          worktrunk = inputs.wrappers.lib.wrapPackage {
+            pkgs = prev;
+            package = prev.worktrunk;
+            env.WORKTRUNK_CONFIG_PATH = tomlFormat.generate "worktrunk-config.toml" {
+              merge = {
+                squash = false;
+                commit = false;
+                rebase = true;
+                remove = false;
+                verify = true;
+                ff = true;
+              };
+              skip-shell-integration-prompt = true;
+              skip-commit-generation-prompt = true;
+            };
+          };
+        })
+      ];
     };
 }
