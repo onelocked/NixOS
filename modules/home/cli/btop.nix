bb6e41eac7f97c049bb3e00af1777b278cdaaf18
onelocked <onelock@mail.com>
2026-05-09 00:23:05 +0100

    btop: turned into a module

--- modules/home/cli/btop.nix
+++ modules/home/cli/btop.nix
@@ -1,75 +1,108 @@
 {
   m.btop =
+    { ... }:
     {
-      wrappers,
+      forte.btop = {
+        settings = {
+          color_theme = "oneshill";
+          vim_keys = true;
+          rounded_corners = true;
+          proc_tree = false;
+          show_gpu_info = "on";
+          show_uptime = true;
+          show_coretemp = true;
+          theme_background = false;
+          cpu_single_graph = true;
+          show_disks = true;
+          show_swap = true;
+          swap_disk = false;
+          use_fstab = false;
+          only_physical = false;
+          shown_boxes = "cpu mem net proc gpu0";
+          gpu_mirror_graph = false;
+        };
+        themes = {
+          oneshill = # bash
+            ''
+              theme[main_bg]="#131316"
+              theme[main_fg]="#e5e1e6"
+              theme[title]="#c5c0ff"
+              theme[hi_fg]="#ebb9d0"
+              theme[selected_bg]="#353438"
+              theme[selected_fg]="#e5e1e6"
+              theme[inactive_fg]="#c8c5d0"
+              theme[proc_misc]="#c7c4dc"
+              theme[cpu_box]="#928f99"
+              theme[mem_box]="#928f99"
+              theme[net_box]="#928f99"
+              theme[proc_box]="#928f99"
+              theme[div_line]="#47464f"
+              theme[temp_start]="#c5c0ff"
+              theme[temp_mid]="#c7c4dc"
+              theme[temp_end]="#ebb9d0"
+              theme[cpu_start]="#c5c0ff"
+              theme[cpu_mid]="#c7c4dc"
+              theme[cpu_end]="#ebb9d0"
+              theme[free_start]="#c5c0ff"
+              theme[free_mid]="#c7c4dc"
+              theme[free_end]="#ebb9d0"
+              theme[cached_start]="#c5c0ff"
+              theme[cached_mid]="#c7c4dc"
+              theme[cached_end]="#ebb9d0"
+              theme[available_start]="#c5c0ff"
+              theme[available_mid]="#c7c4dc"
+              theme[available_end]="#ebb9d0"
+              theme[used_start]="#c5c0ff"
+              theme[used_mid]="#c7c4dc"
+              theme[used_end]="#ebb9d0"
+              theme[download_start]="#c5c0ff"
+              theme[download_mid]="#c7c4dc"
+              theme[download_end]="#ebb9d0"
+              theme[upload_start]="#c5c0ff"
+              theme[upload_mid]="#c7c4dc"
+              theme[upload_end]="#ebb9d0"
+            '';
+        };
+      };
+    };
+  m.default =
+    {
+      lib,
       pkgs,
+      wrappers,
+      config,
       ...
     }:
+    let
+      cfg = config.forte.btop;
+    in
     {
-      hj.packages = [
-        (wrappers.wrappers.btop.wrap {
-          inherit pkgs;
-          settings = {
-            color_theme = "oneshill";
-            vim_keys = true;
-            rounded_corners = true;
-            proc_tree = false;
-            show_gpu_info = "on";
-            show_uptime = true;
-            show_coretemp = true;
-            theme_background = false;
-            cpu_single_graph = true;
-            show_disks = true;
-            show_swap = true;
-            swap_disk = false;
-            use_fstab = false;
-            only_physical = false;
-            shown_boxes = "cpu mem net proc gpu0";
-            gpu_mirror_graph = false;
+      config = lib.mkIf (cfg.enable) {
+        hj.packages = [ cfg.package ];
+      };
+      options.forte.btop = {
+        enable = lib.mkOption {
+          type = lib.types.bool;
+          default = true;
+          description = "Whether to enable btop";
+        };
+
+        settings = lib.mkOption {
+          type = lib.types.attrs;
+          default = { };
+        };
+
+        themes = lib.mkOption {
+          type = lib.types.attrs;
+          default = { };
+        };
+
+        package = lib.mkOption {
+          default = wrappers.wrappers.btop.wrap {
+            inherit pkgs;
+            inherit (cfg) settings themes;
           };
-          themes = {
-            oneshill = # bash
-              ''
-                theme[main_bg]="#131316"
-                theme[main_fg]="#e5e1e6"
-                theme[title]="#c5c0ff"
-                theme[hi_fg]="#ebb9d0"
-                theme[selected_bg]="#353438"
-                theme[selected_fg]="#e5e1e6"
-                theme[inactive_fg]="#c8c5d0"
-                theme[proc_misc]="#c7c4dc"
-                theme[cpu_box]="#928f99"
-                theme[mem_box]="#928f99"
-                theme[net_box]="#928f99"
-                theme[proc_box]="#928f99"
-                theme[div_line]="#47464f"
-                theme[temp_start]="#c5c0ff"
-                theme[temp_mid]="#c7c4dc"
-                theme[temp_end]="#ebb9d0"
-                theme[cpu_start]="#c5c0ff"
-                theme[cpu_mid]="#c7c4dc"
-                theme[cpu_end]="#ebb9d0"
-                theme[free_start]="#c5c0ff"
-                theme[free_mid]="#c7c4dc"
-                theme[free_end]="#ebb9d0"
-                theme[cached_start]="#c5c0ff"
-                theme[cached_mid]="#c7c4dc"
-                theme[cached_end]="#ebb9d0"
-                theme[available_start]="#c5c0ff"
-                theme[available_mid]="#c7c4dc"
-                theme[available_end]="#ebb9d0"
-                theme[used_start]="#c5c0ff"
-                theme[used_mid]="#c7c4dc"
-                theme[used_end]="#ebb9d0"
-                theme[download_start]="#c5c0ff"
-                theme[download_mid]="#c7c4dc"
-                theme[download_end]="#ebb9d0"
-                theme[upload_start]="#c5c0ff"
-                theme[upload_mid]="#c7c4dc"
-                theme[upload_end]="#ebb9d0"
-              '';
-          };
-        })
-      ];
+        };
+      };
     };
 }
