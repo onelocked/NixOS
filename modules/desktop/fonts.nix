f8947578c46c332e039de615d1d4a9237da76813
onelocked <onelock@mail.com>
2026-05-13 15:00:13 +0100

    fonts: use legacyPackages

--- modules/desktop/fonts.nix
+++ modules/desktop/fonts.nix
@@ -1,50 +1,16 @@
 {
-  envoy = {
-    apple-font = {
-      tarball = "https://s3.onelock.org/download/fonts/apple-nerd.tar.gz";
-      locked = true;
-    };
-    apple-font-emoji = {
-      url = "https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji-Linux.ttf";
-      locked = true;
-    };
-  };
   m.desktop =
-    { pkgs, envoy, ... }:
+    { pkgs, self', ... }:
     {
       fonts = {
-        packages =
-          with pkgs;
-          [
-            unifont
-            nerd-fonts.symbols-only
-            montserrat
-            maple-mono.NF
-          ]
-          ++ [
-            (pkgs.stdenvNoCC.mkDerivation {
-              name = envoy.apple-font.pname;
-              inherit (envoy.apple-font) src;
-              dontUnpack = true;
-              dontBuild = true;
-              dontConfigure = true;
-              nativeBuildInputs = [ pkgs.rsync ];
-              installPhase = ''
-                mkdir -p $out/share/fonts/apple-nerd
-                rsync -r --exclude='NY/*Black*' --exclude='NY/*Heavy*' $src/ $out/share/fonts/apple-nerd
-              '';
-            })
-            (pkgs.stdenvNoCC.mkDerivation {
-              name = envoy.apple-font-emoji.pname;
-              inherit (envoy.apple-font-emoji) src;
-              dontUnpack = true;
-              dontBuild = true;
-              dontConfigure = true;
-              installPhase = ''
-                install -D -m644 $src $out/share/fonts/truetype/AppleColorEmoji-Linux.ttf
-              '';
-            })
-          ];
+        packages = with pkgs; [
+          unifont
+          nerd-fonts.symbols-only
+          montserrat
+          maple-mono.NF
+          self'.legacyPackages.apple-font
+          self'.legacyPackages.apple-font-emoji
+        ];
         fontDir.enable = true;
         fontconfig = {
           enable = true;
@@ -64,6 +30,43 @@
             monospace = [ "LigaSFMono Nerd Font" ];
             emoji = [ "Apple Color Emoji" ];
           };
+        };
+      };
+    };
+  envoy = {
+    apple-font = {
+      tarball = "https://s3.onelock.org/download/fonts/apple-nerd.tar.gz";
+      locked = true;
+    };
+    apple-font-emoji = {
+      url = "https://github.com/samuelngs/apple-emoji-linux/releases/latest/download/AppleColorEmoji-Linux.ttf";
+      locked = true;
+    };
+  };
+  perSystem =
+    { pkgs, envoy, ... }:
+    {
+      legacyPackages = {
+        apple-font = pkgs.stdenvNoCC.mkDerivation {
+          name = envoy.apple-font.pname;
+          inherit (envoy.apple-font) src;
+          dontUnpack = true;
+          dontBuild = true;
+          dontConfigure = true;
+          installPhase = ''
+            mkdir -p $out/share/fonts/apple-nerd
+            ${pkgs.rsync}/bin/rsync -r --exclude='NY/*Black*' --exclude='NY/*Heavy*' $src/ $out/share/fonts/apple-nerd
+          '';
+        };
+        apple-font-emoji = pkgs.stdenvNoCC.mkDerivation {
+          name = envoy.apple-font-emoji.pname;
+          inherit (envoy.apple-font-emoji) src;
+          dontUnpack = true;
+          dontBuild = true;
+          dontConfigure = true;
+          installPhase = ''
+            install -D -m644 $src $out/share/fonts/truetype/AppleColorEmoji-Linux.ttf
+          '';
         };
       };
     };
