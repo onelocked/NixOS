197252ccef91e0bb3316e52149a42bd56779ba69
onelocked <onelock@mail.com>
2026-05-19 13:01:29 +0100

    niri: turn into a module of its own

--- modules/desktop/niri/niri.nix
+++ modules/desktop/niri/niri.nix
@@ -10,151 +10,172 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-  perSystem =
-    { inputs', ... }:
-    {
-      packages.niri = inputs'.niri.packages.default.overrideAttrs { doCheck = false; };
+  m.niri = {
+    forte.niri = {
+      enable = true;
+      withUWSM = true;
+      withGreetd = true;
+      withTermFileChooser = true;
+      hotReload = true;
     };
-  m.niri =
+  };
+  m.default =
     {
+      birdee,
       pkgs,
       config,
       lib,
-      birdee,
       self',
       constants,
       ...
     }:
-    lib.mkMerge [
-      {
-        programs.niri = {
-          enable = true;
-          useNautilus = false;
-          package = birdee.wrappers.niri.wrap {
-            inherit pkgs;
-            package = self'.packages.niri;
-            v2-settings = true;
-            disableConfigHotReload = true;
-            inherit (config.forte.niri) settings;
-          };
-        };
-        forte.niri.settings = lib.mkMerge (
-          config.forte.startup
-          |> lib.filter (startup: startup.enable)
-          |> map (startup: {
-            spawn-at-startup = [ startup.spawn ];
-            window-rules = lib.optional (startup.app-id != null) (
-              {
-                matches = [
+    let
+      cfg = config.forte.niri;
+    in
+    {
+      config = lib.mkIf cfg.enable (
+        lib.mkMerge [
+          {
+            hj.packages = [ cfg.package ];
+            xdg.portal = {
+              configPackages = [ cfg.package ];
+              config = {
+                niri = {
+                  default = lib.mkForce [ "gnome" ];
+                  "org.freedesktop.impl.portal.FileChooser" = lib.mkForce (
+                    if cfg.withTermFileChooser then [ "termfilechooser" ] else [ "gtk" ]
+                  );
+                  "org.freedesktop.impl.portal.Secret" = lib.mkForce [ "gnome-keyring" ];
+                  "org.freedesktop.impl.portal.Chooser" = lib.mkForce [ "none" ];
+                };
+              };
+              extraPortals = [
+                pkgs.xdg-desktop-portal-gnome
+              ]
+              ++ lib.optional (!cfg.withTermFileChooser) pkgs.xdg-desktop-portal-gtk;
+            };
+            services.graphical-desktop.enable = true;
+            services.speechd.enable = lib.mkForce false;
+          }
+          (lib.mkIf cfg.withGreetd {
+            services = {
+              displayManager.enable = lib.mkForce false;
+              greetd = {
+                enable = true;
+                settings = {
+                  default_session = {
+                    command =
+                      if cfg.withUWSM then
+                        "${lib.getExe config.programs.uwsm.package} start niri-uwsm.desktop"
+                      else
+                        "${lib.getExe' cfg.package "niri-session"}";
+                    user = constants.username;
+                  };
+                };
+              };
+            };
+          })
+          (lib.mkIf cfg.hotReload {
+            system.userActivationScripts = {
+              niri-reload-config = {
+                text = lib.getExe (
+                  pkgs.writeShellApplication (
+                    let
+                      inherit (cfg) package;
+                      inherit (package.configuration.constructFiles.generatedConfig) outPath;
+                    in
+                    {
+                      name = "niri-reload-config";
+                      runtimeInputs = [
+                        package
+                        pkgs.procps
+                      ];
+                      text = ''
+                        if pgrep -x "niri" > /dev/null; then
+                          niri msg action load-config-file --path "${outPath}"
+                        fi
+                      '';
+                    }
+                  )
+                );
+              };
+            };
+          })
+          (lib.mkIf cfg.withUWSM {
+            forte.xdg.desktopEntries."uuctl".noDisplay = true;
+            programs.uwsm = {
+              enable = true;
+              waylandCompositors = {
+                niri = {
+                  prettyName = "niri";
+                  comment = "Niri compositor managed by UWSM";
+                  binPath = "${lib.getExe cfg.package}";
+                  extraArgs = [ "--session" ];
+                };
+              };
+            };
+          })
+          (lib.mkIf (!cfg.withUWSM) {
+            services.displayManager.sessionPackages = [ cfg.package ];
+          })
+          (lib.mkIf (config.forte.startup != [ ]) {
+            forte.niri.settings = lib.mkMerge (
+              config.forte.startup
+              |> lib.filter (startup: startup.enable)
+              |> map (startup: {
+                spawn-at-startup = [ startup.spawn ];
+                window-rules = lib.optional (startup.app-id != null) (
                   {
-                    inherit (startup) app-id;
+                    matches = [ { inherit (startup) app-id; } ];
                   }
-                ];
-              }
-              // (lib.optionalAttrs (startup.workspace != null) {
-                open-on-workspace = toString startup.workspace;
+                  // (lib.optionalAttrs (startup.workspace != null) {
+                    open-on-workspace = toString startup.workspace;
+                  })
+                  // startup.niriArgs
+                );
               })
-              // startup.niriArgs
             );
           })
-        );
-        system.userActivationScripts = {
-          niri-reload-config = {
-            text = lib.getExe (
-              pkgs.writeShellApplication (
-                let
-                  inherit (config.programs.niri) package;
-                  inherit (package.configuration.constructFiles.generatedConfig) outPath;
-                in
-                {
-                  name = "niri-reload-config";
-                  runtimeInputs = [
-                    package
-                    pkgs.procps
-                  ];
-                  text = ''
-                    if pgrep -x "niri" > /dev/null; then
-                      niri msg action load-config-file --path "${outPath}"
-                    fi
-                  '';
-                }
-              )
-            );
-          };
-        };
-      }
-      (lib.mkIf (config.programs.niri.enable or false) {
-        xdg.portal = {
-          config = {
-            niri = {
-              default = lib.mkForce [ "gnome" ];
-              "org.freedesktop.impl.portal.FileChooser" = lib.mkForce [ "termfilechooser" ];
-              "org.freedesktop.impl.portal.Secret" = lib.mkForce [ "gnome-keyring" ];
-              "org.freedesktop.impl.portal.Chooser" = lib.mkForce [ "none" ];
-            };
-          };
-        };
-
-        forte.xdg.desktopEntries."uuctl".noDisplay = true;
-        programs.uwsm = {
-          enable = true;
-          waylandCompositors = {
-            niri = {
-              prettyName = "niri";
-              comment = "Niri compositor managed by UWSM";
-              binPath = "${lib.getExe config.programs.niri.package}"; # NOTE: /run/current-system/sw/bin/niri is more preferred to avoid version mismatch
-              extraArgs = [ "--session" ];
-            };
-          };
-        };
-        services = {
-          displayManager.enable = lib.mkForce false;
-          greetd = {
-            enable = true;
-            settings = {
-              default_session = {
-                command = "${lib.getExe config.programs.uwsm.package} start niri-uwsm.desktop";
-                user = constants.username;
-              };
-            };
-          };
-        };
-      })
-    ];
-  m.default =
-    { lib, ... }:
-    {
+        ]
+      );
       options = {
-        forte.startup = lib.mkOption {
-          description = "Programs to run on startup";
-          default = [ ];
-          type = lib.types.listOf (
-            lib.types.submodule {
-              options = {
-                app-id = lib.mkOption {
-                  type = lib.types.nullOr lib.types.str;
-                  default = null;
-                };
-                enable = lib.mkEnableOption "Startup program" // {
-                  default = true;
-                };
-                spawn = lib.mkOption {
-                  type = lib.types.listOf lib.types.str;
-                };
-                workspace = lib.mkOption {
-                  type = lib.types.nullOr (lib.types.either lib.types.int lib.types.str);
-                  default = null;
-                };
-                niriArgs = lib.mkOption {
-                  type = lib.types.attrs;
-                  default = { };
-                };
-              };
-            }
-          );
-        };
-
         forte.niri = {
+          enable = lib.mkEnableOption "Niri, a scrollable-tiling Wayland compositor";
+          package = lib.mkOption {
+            type = lib.types.package;
+            default = birdee.wrappers.niri.wrap {
+              inherit pkgs;
+              package = self'.packages.niri;
+              v2-settings = true;
+              disableConfigHotReload = true;
+              inherit (config.forte.niri) settings;
+            };
+            defaultText = lib.literalExpression "wrapped niri";
+            description = "Niri package to use.";
+          };
+          withUWSM = lib.mkEnableOption null // {
+            description = ''
+              Launch Niri with the UWSM (Universal Wayland Session Manager) session manager.
+              This has improved systemd support.
+              This automatically starts appropriate targets like `graphical-session.target`,
+              and `wayland-session@Niri.target`.
+            '';
+          };
+          withGreetd = lib.mkEnableOption null // {
+            description = ''
+              Whether to enable greetd as the login manager for the niri Wayland compositor,
+            '';
+          };
+          withTermFileChooser = lib.mkEnableOption null // {
+            description = ''
+              Whether to enable xdg-termfilechooser settings for niri,
+            '';
+          };
+          hotReload = lib.mkEnableOption null // {
+            description = ''
+              Whether to enable hot-reloading of niri config.
+            '';
+          };
           settings = lib.mkOption {
             default = { };
             type = lib.types.submodule {
@@ -197,6 +218,39 @@
             };
           };
         };
+        forte.startup = lib.mkOption {
+          description = "Programs to run on startup";
+          default = [ ];
+          type = lib.types.listOf (
+            lib.types.submodule {
+              options = {
+                app-id = lib.mkOption {
+                  type = lib.types.nullOr lib.types.str;
+                  default = null;
+                };
+                enable = lib.mkEnableOption "Startup program" // {
+                  default = true;
+                };
+                spawn = lib.mkOption {
+                  type = lib.types.listOf lib.types.str;
+                };
+                workspace = lib.mkOption {
+                  type = lib.types.nullOr (lib.types.either lib.types.int lib.types.str);
+                  default = null;
+                };
+                niriArgs = lib.mkOption {
+                  type = lib.types.attrs;
+                  default = { };
+                };
+              };
+            }
+          );
+        };
       };
+    };
+  perSystem =
+    { inputs', ... }:
+    {
+      packages.niri = inputs'.niri.packages.default.overrideAttrs { doCheck = false; };
     };
 }
