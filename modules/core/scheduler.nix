0ec77bc3addd2e758fc6b84a61c9fcf773fe297f
onelocked <onelock@mail.com>
2026-05-19 14:48:43 +0100

    system76 scheduler move into gaming.nix

--- modules/core/scheduler.nix
+++ modules/core/scheduler.nix
@@ -3,14 +3,10 @@
     { lib, pkgs, ... }:
     {
       services = {
-        system76-scheduler = {
-          enable = true;
-          useStockConfig = true;
-        };
         scx = {
           enable = true;
           package = pkgs.scx.rustscheds;
-          scheduler = lib.mkDefault "scx_lavd"; # Gaming scheduler https://github.com/sched-ext/scx/blob/main/scheds/rust/README.md
+          scheduler = lib.mkDefault "scx_bpfland"; # scheduler that prioritizes interactive workloads  https://github.com/sched-ext/scx/blob/main/scheds/rust/README.md
         };
       };
     };
