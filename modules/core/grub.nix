24623a683bd17f2cdaa2686d6d723679cc5840e9
onelocked <onelock@mail.com>
2026-05-23 03:16:59 +0100

    grub: replace the image path with a url

--- modules/core/grub.nix
+++ modules/core/grub.nix
@@ -4,40 +4,36 @@
     locked = true;
   };
   m.default =
+    { pkgs, envoy, ... }:
     {
-      pkgs,
-      envoy,
-      constants,
-      ...
-    }:
-    {
-      boot.loader =
-        let
-          inherit (constants) homedir;
-        in
-        {
-          efi = {
-            canTouchEfiVariables = true;
-            efiSysMountPoint = "/boot";
-          };
-          timeout = 1;
-          grub = {
-            enable = true;
-            device = "nodev";
-            efiSupport = true;
-            useOSProber = true;
-            gfxmodeEfi = "3440x1440";
-            gfxmodeBios = "3440x1440";
-            splashMode = "normal";
-            splashImage = homedir + "/Pictures/grub.png";
-            theme = pkgs.stdenv.mkDerivation {
-              inherit (envoy.grubTheme) pname src version;
-              installPhase = ''
-                install -dm755 $out
-                cp -rf theme/* $out/
-              '';
-            };
+      boot.loader = {
+        efi = {
+          canTouchEfiVariables = true;
+          efiSysMountPoint = "/boot";
+        };
+        timeout = 1;
+        grub = {
+          enable = true;
+          device = "nodev";
+          efiSupport = true;
+          useOSProber = true;
+          gfxmodeEfi = "3440x1440";
+          gfxmodeBios = "3440x1440";
+          splashMode = "normal";
+          splashImage = (
+            pkgs.fetchurl {
+              url = "https://raw.githubusercontent.com/onelocked/images/refs/heads/main/grubmeath.png";
+              hash = "sha256-/OrtuIi3y4eihIF8FoBtjy+0ykTYRGA0uHyp0PAAu/o=";
+            }
+          );
+          theme = pkgs.stdenv.mkDerivation {
+            inherit (envoy.grubTheme) pname src version;
+            installPhase = ''
+              install -dm755 $out
+              cp -rf theme/* $out/
+            '';
           };
         };
+      };
     };
 }
