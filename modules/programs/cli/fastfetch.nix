abc124be37978e815b9a7a3bf74cc76e73651344
onelocked <onelock@mail.com>
2026-05-26 14:28:00 +0100

    fastfetch: turn into a module

--- modules/programs/cli/fastfetch.nix
+++ modules/programs/cli/fastfetch.nix
@@ -1,99 +1,126 @@
 {
   m.fastfetch =
+    let
+      esc = (builtins.fromJSON ''{ "value": "\u001b" }'').value;
+    in
     {
-      pkgs,
+      forte.fastfetch = {
+        enable = true;
+        settings = {
+          "$schema" = "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json";
+          logo = {
+            source = "-";
+            type = "raw";
+            width = 20;
+            padding = {
+              top = 2;
+              left = 2;
+            };
+          };
+          display = {
+            separator = " ┈➤ ";
+          };
+          modules = [
+            "break"
+            {
+              type = "title";
+              keyWidth = 10;
+              format = "         ${esc}[38;2;197;192;255m{1}${esc}[38;2;168;200;240m@${esc}[38;2;200;176;232m{2}${esc}[0m";
+            }
+            {
+              type = "custom";
+              format = " ─────────────────────────── ";
+            }
+            {
+              type = "os";
+              key = " ";
+              keyColor = "#cfd3e7";
+            }
+            {
+              type = "cpu";
+              key = " ";
+              keyColor = "#f4a8b8";
+            }
+            {
+              type = "gpu";
+              key = "󰢮 ";
+              keyColor = "#ff7a6b";
+            }
+            {
+              type = "memory";
+              key = " ";
+              keyColor = "#b8db8c";
+              format = "{1} / {2}";
+            }
+            {
+              type = "wm";
+              key = " ";
+              keyColor = "#7d75c0";
+            }
+            {
+              type = "terminal";
+              key = " ";
+              keyColor = "#f6d88a";
+            }
+            {
+              type = "custom";
+              format = " ─────────────────────────── ";
+            }
+            {
+              type = "custom";
+              format = "   ${esc}[31m  ${esc}[32m  ${esc}[33m  ${esc}[34m  ${esc}[35m  ${esc}[36m  ${esc}[37m  ${esc}[90m ";
+            }
+            "break"
+          ];
+        };
+      };
+    };
+  m.default =
+    {
       lib,
       birdee,
+      pkgs,
       config,
       ...
     }:
     let
-      fastfetch-config =
-        pkgs.writeText "config.jsonc" # json
-          ''
-            {
-              "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
-              "logo": {
-                "source": "-",
-                "type": "raw",
-                "width": 20,
-                "padding": {
-                  "top": 2,
-                  "left": 2
-                }
-              },
-              "display": {
-                "separator": " ┈➤ "
-              },
-              "modules": [
-                "break",
-                {
-                  "type": "title",
-                  "keyWidth": 10,
-                  "format": "         \u001b[38;2;197;192;255m{1}\u001b[38;2;168;200;240m@\u001b[38;2;200;176;232m{2}\u001b[0m"
-                },
-                {
-                  "type": "custom",
-                  "format": " ─────────────────────────── "
-                },
-                {
-                  "type": "os",
-                  "key": " ",
-                  "keyColor": "#cfd3e7"
-                },
-                {
-                  "type": "cpu",
-                  "key": " ",
-                  "keyColor": "#f4a8b8"
-                },
-                {
-                  "type": "gpu",
-                  "key": "󰢮 ",
-                  "keyColor": "#ff7a6b"
-                },
-                {
-                  "type": "memory",
-                  "key": " ",
-                  "keyColor": "#b8db8c",
-                  "format": "{1} / {2}"
-                },
-                {
-                  "type": "wm",
-                  "key": " ",
-                  "keyColor": "#7d75c0"
-                },
-                {
-                  "type": "terminal",
-                  "key": " ",
-                  "keyColor": "#f6d88a"
-                },
-                {
-                  "type": "custom",
-                  "format": " ─────────────────────────── "
-                },
-                {
-                  "type": "custom",
-                  "format": "   \u001b[31m  \u001b[32m  \u001b[33m  \u001b[34m  \u001b[35m  \u001b[36m  \u001b[37m  \u001b[90m "
-                },
-                "break"
-              ]
-            }
-          '';
-
+      cfg = config.forte.fastfetch;
+      json = pkgs.formats.json { };
     in
     {
-      forte.lib.fastfetch.package = birdee.lib.wrapPackage {
-        inherit pkgs;
-        package = pkgs.fastfetch.minimal;
-        flags = {
-          "--config" = "${fastfetch-config}";
+      config = lib.mkIf cfg.enable {
+        environment.shellAliases.ff = "kitten icat -n --place 20x20@2x1 --scale-up --align left ${
+          (pkgs.fetchurl {
+            url = "https://raw.githubusercontent.com/onelocked/images/refs/heads/main/fleet-snowfluff.gif";
+            hash = "sha256-Vz6QZrhr5c+ShiHJwxHFeyCXszWFvDjhKFm2CyQNAbo=";
+          })
+        } | ${lib.getExe cfg.package}";
+      };
+
+      options.forte.fastfetch = {
+        enable = lib.mkEnableOption "fastfetch";
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = birdee.lib.wrapPackage (
+            { config, ... }:
+            {
+              inherit pkgs;
+              package = pkgs.fastfetch.minimal;
+              flags = {
+                "--config" = config.constructFiles.generatedConfig.path;
+              };
+              constructFiles.generatedConfig = {
+                relPath = "config.jsonc";
+                builder = ''mkdir -p "$(dirname "$2")" && cp ${json.generate "config.jsonc" cfg.settings} "$2"'';
+              };
+            }
+          );
+        };
+        settings = lib.mkOption {
+          inherit (json) type;
+          default = { };
+          description = "Fastfetch config options";
         };
       };
-      environment.shellAliases.ff = "kitten icat -n --place 20x20@2x1 --scale-up --align left ${
-        (pkgs.fetchurl {
-          url = "https://raw.githubusercontent.com/onelocked/images/refs/heads/main/fleet-snowfluff.gif";
-          hash = "sha256-Vz6QZrhr5c+ShiHJwxHFeyCXszWFvDjhKFm2CyQNAbo=";
-        })
-      } | ${lib.getExe config.forte.lib.fastfetch.package}";
     };
 }
