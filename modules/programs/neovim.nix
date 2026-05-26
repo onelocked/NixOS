13e5fe9c8618dfab1dc0509ca963df574e2e11fa
onelocked <onelock@mail.com>
2026-05-26 15:25:38 +0100

    neovim: turned into a module

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -7,48 +7,80 @@
       systems.follows = "systems";
     };
   };
-  m.neovim =
-    { inputs', ... }:
+  m.neovim = {
+    forte.neovim = {
+      enable = true;
+      defaultEditor = true;
+      desktopEntry = true;
+    };
+  };
+  m.default =
     {
-      hj = {
-        packages = [ inputs'.vimmax.packages.default ];
-        environment.sessionVariables = {
-          EDITOR = "nvim";
-          VISUAL = "nvim";
-        };
-      };
-
-      forte.xdg.desktopEntries = {
-        "nvim" = {
-          name = "Neovim";
-          noDisplay = true;
-          genericName = "Text Editor";
-          comment = "Edit text files";
-          exec = "nvim %F";
-          terminal = true;
-          type = "Application";
-          icon = "nvim";
-          startupNotify = false;
-          mimeType = [
-            "text/english"
-            "text/plain"
-            "text/x-makefile"
-            "text/x-c++hdr"
-            "text/x-c++src"
-            "text/x-chdr"
-            "text/x-csrc"
-            "text/x-java"
-            "text/x-moc"
-            "text/x-pascal"
-            "text/x-tcl"
-            "text/x-tex"
-            "application/x-shellscript"
-            "text/x-c"
-            "text/x-c++"
-          ];
-          settings = {
-            TryExec = "nvim";
-          };
+      lib,
+      inputs',
+      config,
+      ...
+    }:
+    let
+      cfg = config.forte.neovim;
+    in
+    {
+      config = lib.mkIf cfg.enable (
+        lib.mkMerge [
+          {
+            hj.packages = [ cfg.package ];
+          }
+          (lib.mkIf cfg.defaultEditor {
+            environment.sessionVariables = {
+              EDITOR = "nvim";
+              VISUAL = "nvim";
+            };
+          })
+          (lib.mkIf cfg.desktopEntry {
+            forte.xdg.desktopEntries = {
+              "nvim" = {
+                name = "Neovim";
+                noDisplay = true;
+                genericName = "Text Editor";
+                comment = "Edit text files";
+                exec = "nvim %F";
+                terminal = true;
+                type = "Application";
+                icon = "nvim";
+                startupNotify = false;
+                mimeType = [
+                  "text/english"
+                  "text/plain"
+                  "text/x-makefile"
+                  "text/x-c++hdr"
+                  "text/x-c++src"
+                  "text/x-chdr"
+                  "text/x-csrc"
+                  "text/x-java"
+                  "text/x-moc"
+                  "text/x-pascal"
+                  "text/x-tcl"
+                  "text/x-tex"
+                  "application/x-shellscript"
+                  "text/x-c"
+                  "text/x-c++"
+                ];
+                settings = {
+                  TryExec = "nvim";
+                };
+              };
+            };
+          })
+        ]
+      );
+      options.forte.neovim = {
+        enable = lib.mkEnableOption "neovim";
+        defaultEditor = lib.mkEnableOption "defaultEditor";
+        desktopEntry = lib.mkEnableOption "desktopEntry";
+        package = lib.mkOption {
+          type = lib.types.package;
+          default = inputs'.vimmax.packages.default;
+          defaultText = "default package for neovim";
         };
       };
     };
