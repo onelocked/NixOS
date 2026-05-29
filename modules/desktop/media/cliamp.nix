0e4be7056ead4d6d0c02524758b30ad85513cb47
onelocked <onelock@mail.com>
2026-05-29 15:50:09 +0100

    cliamp: conditional based on theme

--- modules/desktop/media/cliamp.nix
+++ modules/desktop/media/cliamp.nix
@@ -8,22 +8,27 @@
     }:
     let
       cfg = config.forte.otter-launcher;
+      theme = config.forte.theme.variant;
     in
     {
       config = lib.mkIf cfg.enable {
         forte.lib.otter-lib.cliamp-config = pkgs.writeText "otter-kitty.conf" ''
-          allow_remote_control yes
-          background_opacity 1
-          background_image        ${
-            (pkgs.fetchurl {
-              url = "https://raw.githubusercontent.com/onelocked/images/refs/heads/main/aemeath-player.png";
-              hash = "sha256-IjywuR2gVYJK+6wwdV5bd48hjWLXgUKMOvApIHp+Ig0=";
-            })
-          }
-          background_image_layout scaled
-          background_image_linear yes
-          font_size               12
-          window_padding_width    170 30 170 30
+            allow_remote_control yes
+            background_opacity 1
+          ${
+            lib.optionalString (theme == "dark") ''
+              background_image        ${
+                (pkgs.fetchurl {
+                  url = "https://raw.githubusercontent.com/onelocked/images/refs/heads/main/aemeath-player.png";
+                  hash = "sha256-IjywuR2gVYJK+6wwdV5bd48hjWLXgUKMOvApIHp+Ig0=";
+                })
+              }
+              background_image_layout scaled
+              background_image_linear yes
+              font_size               12
+              window_padding_width    170 30 170 30
+            ''
+          };
         '';
 
         forte.otter-launcher.modules = [
@@ -34,12 +39,18 @@
           }
         ];
         forte.niri.settings.window-rules = [
-          {
-            matches = [ { app-id = "^CliampMusic$"; } ];
-            open-floating = true;
-            default-column-width.fixed = 780;
-            default-window-height.fixed = 1090;
-          }
+          (
+            {
+              matches = [ { app-id = "^CliampMusic$"; } ];
+              open-floating = true;
+              default-column-width.fixed = 780;
+              default-window-height.fixed = 815;
+            }
+            // lib.optionalAttrs (theme == "dark") {
+              default-column-width.fixed = 780;
+              default-window-height.fixed = 1090;
+            }
+          )
         ];
       };
     };
