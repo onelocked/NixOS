b204dd28d5381273caa0221e6c286f2c80da66f1
onelocked <onelock@mail.com>
2026-05-31 20:18:15 +0100

    yazi: use nix run instead of pulling the package directly

--- modules/programs/tui/yazi/yazi-opener.nix
+++ modules/programs/tui/yazi/yazi-opener.nix
@@ -1,11 +1,14 @@
 {
   m.yazi =
     {
-      pkgs,
       lib,
       self',
+      pkgs,
       ...
     }:
+    let
+      run = "${pkgs.nix}/bin/nix run";
+    in
     {
       forte.yazi.settings = {
         open = {
@@ -14,42 +17,40 @@
               mime = "image/*"; # Apply this to all image types
               use = [
                 "open"
-                "setwallpaper"
-                "loupe"
-                "nomacs"
+                "images"
               ];
             }
             {
               mime = "video/*"; # Apply this to all video types
               use = [
                 "open"
-                "video-trimmer"
+                "videos"
               ];
             }
           ];
         };
-        opener =
-          let
-            inherit (lib) getExe;
-            mkOpener = pkg: desc: [
-              {
-                run = "${getExe pkg} %s";
-                inherit desc;
-              }
-            ];
-          in
-          with pkgs;
-          {
-            setwallpaper = [
-              {
-                run = "${getExe awww} img %s --transition-duration 2 --transition-fps 60 --transition-bezier .43,1.19,1,.4 --transition-type any";
-                desc = "Set Wallpaper";
-              }
-            ];
-            loupe = mkOpener self'.packages.loupe "Loupe";
-            nomacs = mkOpener nomacs "Image Editor";
-            video-trimmer = mkOpener video-trimmer "Video Trimmer";
-          };
+        opener = {
+          images = [
+            {
+              run = "${pkgs.awww}/bin/awww img %s --transition-duration 2 --transition-fps 60 --transition-bezier .43,1.19,1,.4 --transition-type any";
+              desc = "Set Wallpaper";
+            }
+            {
+              run = "${run} nixpkgs#gimp %s";
+              desc = "Edit with Gimp";
+            }
+            {
+              run = "${lib.getExe self'.packages.loupe} %s";
+              desc = "Open with Loupe";
+            }
+          ];
+          videos = [
+            {
+              run = "${run} nixpkgs#video-trimmer %s";
+              desc = "Videos Trimmer";
+            }
+          ];
+        };
       };
     };
   perSystem =
