34dab365650c9c621a058e78a8f45d0a5bdf1ef9
onelocked <onelock@mail.com>
2026-06-01 22:00:20 +0100

    ddcutil: use the module

--- modules/desktop/ddcutil.nix
+++ modules/desktop/ddcutil.nix
@@ -1,14 +1,8 @@
 {
   m.desktop =
-    { pkgs, constants, ... }:
+    { pkgs, ... }:
     {
       environment.systemPackages = with pkgs; [ ddcutil ];
-
-      services.udev.extraRules = ''
-        KERNEL=="i2c-[0-9]*", GROUP="i2c", MODE="0660"
-      '';
-      boot.kernelModules = [ "i2c-dev" ];
-      users.groups.i2c = { };
-      users.users.${constants.username}.extraGroups = [ "i2c" ];
+      services.ddccontrol.enable = true;
     };
 }
