3a7d968154038710fb275e0136c334a4b613af29
onelocked <onelock@mail.com>
2026-06-13 21:48:49 +0100

    nix: remove garnix

--- modules/core/kernel.nix
+++ modules/core/kernel.nix
@@ -12,11 +12,9 @@
     {
       nix.settings = {
         substituters = [
-          "https://cache.garnix.io"
           "https://attic.xuyh0120.win/lantian"
         ];
         trusted-public-keys = [
-          "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
           "lantian:EeAUQ+W+6r7EtwnmYjeVwx5kOGEBpjlBfPlzGlTNvHc="
         ];
       };
