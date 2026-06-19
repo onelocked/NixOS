fa05750d463cc0a552a6bb6e6514476699548164
onelocked <onelock@mail.com>
2026-06-19 15:21:57 +0100

    qt: module refactor

--- modules/desktop/gtk-qt/qt.nix
+++ modules/desktop/gtk-qt/qt.nix
@@ -2,70 +2,140 @@
   exo.mods.desktop =
     {
       config,
-      pkgs,
-      lib,
       scheme,
       ...
     }:
     let
-      iniFmt = (pkgs.formats.ini { }).generate;
-
-      qtctConf = {
-        Appearance = {
-          color_scheme_path = colorSchemeFile;
-          custom_palette = true;
-          icon_theme = config.forte.gtk.icons.name;
-          standard_dialogs = "default";
-          style = "Adwaita-Dark";
-        };
-        Fonts = {
-          fixed = font;
-          general = font;
-        };
-      };
-
-      font = ''"${config.forte.gtk.font.serif},12,-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'';
-
-      qtctFile.source = iniFmt "qtct.conf" qtctConf;
-
-      palette =
-        with scheme.withHashtag;
-        "${base07}, ${base00}, #ffffff, #cacaca, #9f9f9f, #b8b8b8, ${base07}, #ffffff, ${base07}, ${base00}, ${base00}, #000000, ${base0D}, ${base06}, ${base05}, ${base0F}, ${base03}, ${base00}, ${base03}, ${base07}, ${base07}, ${base0F}";
-      colorSchemeFile = iniFmt "colors.conf" {
-        ColorScheme =
-          [
-            "active_colors"
-            "disabled_colors"
-            "inactive_colors"
-          ]
-          |> (names: lib.genAttrs names (_: palette));
-      };
+      gtk = config.forte.gtk;
     in
     {
-      environment = {
-        systemPackages = with pkgs.qt6Packages; [
-          qt6ct
-          qtwayland
+      forte.qt = {
+        enable = true;
+        settings = {
+          Appearance = {
+            icon_theme = gtk.icons.name;
+            standard_dialogs = "default";
+            style = "Adwaita-Dark";
+          };
+        };
+        font = {
+          name = gtk.font.serif;
+          size = gtk.font.size;
+        };
+        palette = with scheme.withHashtag; [
+          base07
+          base00
+          "#ffffff"
+          "#cacaca"
+          "#9f9f9f"
+          "#b8b8b8"
+          base07
+          "#ffffff"
+          base07
+          base00
+          base00
+          "#000000"
+          base0D
+          base06
+          base05
+          base0F
+          base03
+          base00
+          base03
+          base07
+          base07
+          base0F
         ];
-        sessionVariables = {
+      };
+    };
+
+  exo.skeleton =
+    {
+      config,
+      pkgs,
+      lib,
+      ...
+    }:
+    let
+      cfg = config.forte.qt;
+      ini = (pkgs.formats.ini { }).generate;
+    in
+    {
+      config = lib.mkIf cfg.enable {
+        qt = {
+          enable = true;
+          platformTheme = "qt5ct";
+          style = "adwaita-dark";
+        };
+
+        environment.sessionVariables = {
           QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
         };
+
+        hj.xdg.config.files =
+          lib.genAttrs
+            [
+              "qt5ct/qt5ct.conf"
+              "qt6ct/qt6ct.conf"
+            ]
+            (_: {
+              source = ini "qtct.conf" cfg.settings;
+            });
+
+        forte.xdg.desktopEntries = lib.genAttrs [ "qt5ct" "qt6ct" ] (_: {
+          noDisplay = true;
+        });
       };
 
-      forte.xdg.desktopEntries = {
-        "qt5ct".noDisplay = true;
-        "qt6ct".noDisplay = true;
-      };
+      options.forte.qt = {
+        enable = lib.mkEnableOption "Qt";
 
-      qt = {
-        enable = true;
-        platformTheme = "qt5ct";
-        style = "adwaita-dark";
-      };
+        settings = lib.mkOption {
+          type = lib.types.attrs;
+          default = { };
+          apply =
+            userSettings:
+            let
+              fName = cfg.font.name;
+              fSize = cfg.font.size;
+              qtFontString = ''"${fName},${fSize},-1,5,400,0,0,0,0,0,0,0,0,0,0,1"'';
+            in
+            (lib.recursiveUpdate {
+              Appearance = {
+                color_scheme_path = cfg.palette;
+                custom_palette = true;
+              };
+            } userSettings)
+            // {
+              Fonts = (userSettings.Fonts or { }) // {
+                fixed = qtFontString;
+                general = qtFontString;
+              };
+            };
+        };
+        font = {
+          name = lib.mkOption {
+            type = lib.types.str;
+            default = config.forte.gtk.font.serif;
+          };
+          size = lib.mkOption {
+            type = lib.types.str;
+            default = "12";
+          };
+        };
 
-      hj.xdg.config.files = {
-        "qt5ct/qt5ct.conf" = qtctFile;
-        "qt6ct/qt6ct.conf" = qtctFile;
+        palette = lib.mkOption {
+          type = lib.types.listOf lib.types.str;
+          description = "List of 22 color hex codes for the Qt palette.";
+          default = [ ];
+          apply =
+            colors:
+            ini "colors.conf" {
+              ColorScheme = lib.genAttrs [ "active_colors" "disabled_colors" "inactive_colors" ] (
+                _: lib.concatStringsSep ", " colors
+              );
+            };
+        };
       };
     };
 }
