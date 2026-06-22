14b866de6fb6eba0bb39e93c0a26b99043d4f14d
onelocked <180497738+onelocked@users.noreply.github.com>
2026-06-22 21:26:56 +0100

    amdgpu_top: make a wrapper

--- hosts/mini-pc/hardware/amd-gpu.nix
+++ hosts/mini-pc/hardware/amd-gpu.nix
@@ -1,29 +1,26 @@
 {
   exo.hardware.mini-pc =
+    { self', ... }:
     {
-      pkgs,
-      lib,
-      self',
-      ...
-    }:
-    {
-      environment = {
-        systemPackages = with pkgs; [ rocmPackages.amdsmi ];
-        shellAliases.gtop = "${lib.getExe self'.packages.amdgpu_top} --dark";
-      };
       nixpkgs.config.rocmSupport = true;
       hardware.amdgpu.opencl.enable = true;
+      hj.packages = [ self'.packages.amdgpu_top ];
     };
   perSystem =
     { pkgs, ... }:
     {
-      packages.amdgpu_top = pkgs.amdgpu_top.overrideAttrs (oldAttrs: {
+      packages.amdgpu_top = pkgs.amdgpu_top.overrideAttrs (old: {
         doCheck = false;
-        cargoBuildFlags = (oldAttrs.cargoBuildFlags or [ ]) ++ [
+        cargoBuildFlags = (old.cargoBuildFlags or [ ]) ++ [
           "--no-default-features"
           "--features"
           "tui,libamdgpu_top/libdrm_link"
         ];
+        nativeBuildInputs = (old.nativeBuildInputs or [ ]) ++ [ pkgs.makeWrapper ];
+        postInstall = (old.postInstall or "") + ''
+          makeWrapper $out/bin/amdgpu_top $out/bin/gtop \
+            --add-flags '--dark'
+        '';
       });
     };
 }
