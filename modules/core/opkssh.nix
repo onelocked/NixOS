cefe473ee4f920b1f48c378bee1ea375795ab353
onelocked <180497738+onelocked@users.noreply.github.com>
2026-06-27 14:15:17 +0100

    opkssh: turned into a module

--- modules/core/opkssh.nix
+++ modules/core/opkssh.nix
@@ -4,37 +4,47 @@
       pkgs,
       config,
       constants,
+      lib,
+      hostName,
       ...
     }:
     let
+      cfg = config.forte.opkssh;
       inherit (constants) username homedir;
     in
     {
-      hj.packages = [ pkgs.opkssh ];
-      sops = {
-        secrets.client_id.owner = username;
-        secrets.pocket_id_issuer.owner = username;
-        templates."opkssh-config.yml" = {
-          content = # yaml
-            with config.sops.placeholder; ''
-              ---
-              default_provider: PocketID
+      config = lib.mkIf cfg.enable {
+        hj.packages = [ pkgs.opkssh ];
+        sops = {
+          secrets.client_id.owner = username;
+          secrets.pocket_id_issuer.owner = username;
+          templates."opkssh-config.yml" = {
+            content = # yaml
+              with config.sops.placeholder; ''
+                ---
+                default_provider: PocketID
 
-              providers:
-                - alias: PocketID
-                  issuer: ${pocket_id_issuer}
-                  client_id: ${client_id}
-                  scopes: openid email profile
-                  access_type: offline
-                  use_pkce: true
-                  prompt: consent
-                  redirect_uris:
-                    - http://localhost:3000/login-callback
-                    - http://localhost:10001/login-callback
-                    - http://localhost:11110/login-callback
-            '';
-          path = homedir + "/.opk/config.yml";
-          owner = username;
+                providers:
+                  - alias: PocketID
+                    issuer: ${pocket_id_issuer}
+                    client_id: ${client_id}
+                    scopes: openid email profile
+                    access_type: offline
+                    use_pkce: true
+                    prompt: consent
+                    redirect_uris:
+                      - http://localhost:3000/login-callback
+                      - http://localhost:10001/login-callback
+                      - http://localhost:11110/login-callback
+              '';
+            path = homedir + "/.opk/config.yml";
+            owner = username;
+          };
+        };
+      };
+      options.forte.opkssh = {
+        enable = lib.mkEnableOption null // {
+          default = if hostName != "gaming-pc" then true else false;
         };
       };
     };
