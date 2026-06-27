65ca574518c5d4da571e777647db484312f3b066
onelocked <180497738+onelocked@users.noreply.github.com>
2026-06-27 19:02:50 +0100

    flatpak: enabled only for gaming pc with correct preservation

--- modules/desktop/flatpak.nix
+++ modules/desktop/flatpak.nix
@@ -7,16 +7,18 @@
       ...
     }:
     {
-      services.flatpak.enable = if hostName == "gaming-pc" then true else false;
-      preservation.preserveAt = {
-        "/games" = {
-          commonMountOptions = [ "x-gvfs-hide" ];
-          directories = lib.unique [ "/var/lib/flatpak" ];
-          users.${constants.username}.directories = lib.unique [
-            ".local/share/flatpak"
-            ".cache/flatpak"
-            ".var/app"
-          ];
+      config = lib.mkIf (hostName == "gaming-pc") {
+        services.flatpak.enable = true;
+        preservation.preserveAt = {
+          "/games" = {
+            commonMountOptions = [ "x-gvfs-hide" ];
+            directories = lib.unique [ "/var/lib/flatpak" ];
+            users.${constants.username}.directories = lib.unique [
+              ".local/share/flatpak"
+              ".cache/flatpak"
+              ".var/app"
+            ];
+          };
         };
       };
     };
