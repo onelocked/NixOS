6ca33900b48358696d131182cc30b2dcfd906a65
onelocked <180497738+onelocked@users.noreply.github.com>
2026-06-28 12:06:22 +0100

    hosts: nfs share for gaming-pc

--- hosts/gaming-pc/gaming-pc.nix
+++ hosts/gaming-pc/gaming-pc.nix
@@ -8,23 +8,32 @@
         desktop
         gaming
       ];
-      extraConfig = {
-        desktop.media.enable = false;
-        forte.quickshell.enable = false;
+      extraConfig =
+        { constants, ... }:
+        {
+          desktop.media.enable = false;
+          forte.quickshell.enable = false;
 
-        services.openssh = {
-          enable = true;
-          settings = {
-            PermitRootLogin = "no";
-            PasswordAuthentication = false;
-            PermitEmptyPasswords = false;
+          services.openssh = {
+            enable = true;
+            settings = {
+              PermitRootLogin = "no";
+              PasswordAuthentication = false;
+              PermitEmptyPasswords = false;
+            };
           };
-        };
 
-        users.users.onelock.openssh.authorizedKeys.keys = [
-          "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICM7ifW7zlpT8VeWOgCpKSAdnHr4vgIzrcyId/RQ822J gaming-pc"
-        ];
-      };
+          users.users.onelock.openssh.authorizedKeys.keys = [
+            "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICM7ifW7zlpT8VeWOgCpKSAdnHr4vgIzrcyId/RQ822J gaming-pc"
+          ];
+          services.nfs.server.enable = true;
+
+          services.nfs.server.exports = ''
+            ${constants.homedir}/Documents/NFS-Share  192.168.1.185/32(rw,sync,no_subtree_check,no_root_squash)
+          '';
+
+          networking.firewall.allowedTCPPorts = [ 2049 ];
+        };
     };
   };
 }
