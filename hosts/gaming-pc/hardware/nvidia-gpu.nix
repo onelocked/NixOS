a05c6466427cbab8e85c093921c95d27276c59a8
onelocked <180497738+onelocked@users.noreply.github.com>
2026-07-01 14:11:49 +0100

    gaming-pc: nvidia improved settings

--- hosts/gaming-pc/hardware/nvidia-gpu.nix
+++ hosts/gaming-pc/hardware/nvidia-gpu.nix
@@ -4,19 +4,23 @@
       enable = true;
       enable32Bit = true;
     };
-    services.xserver.videoDrivers = [ "nvidia" ];
     hardware.nvidia = {
       branch = "bleeding_edge";
       modesetting.enable = true;
       open = true;
-      nvidiaSettings = true;
+      nvidiaSettings = false;
     };
-    services.lact.enable = true;
+    services.xserver.videoDrivers = [ "nvidia" ]; # needed to  have nviida drivers enabled
+    forte.allowUnfree = [ "nvidia-x11" ];
+
+    services.lact.enable = true; # GPU fan control GUI with a daemon
+
     environment.sessionVariables = {
       LIBVA_DRIVER_NAME = "nvidia";
       __GLX_VENDOR_LIBRARY_NAME = "nvidia";
     };
-    boot.kernelModules = [ "ntsync" ];
+
+    boot.kernelModules = [ "ntsync" ]; # support driver for emulation of NT synchronization, used by Wine/Proton
     forte.persist.root.directories = [ "/etc/lact" ];
   };
 }
