f16e1e190560a760c27ecd3d6c086587f7b86781
onelocked <180497738+onelocked@users.noreply.github.com>
2026-07-09 17:50:11 +0100

    feat(nix): use tack for  input pinning instead of flake.lock

--- flake.nix
+++ flake.nix
@@ -1,9 +1,22 @@
 {
   outputs =
-    inputs:
-    inputs.flake-parts.lib.evalFlakeModule { inherit inputs; } {
-      imports =
-        with inputs.nixpkgs.lib;
+    { self, ... }:
+    let
+      tackInputs = import ./.tack;
+
+      inherit (tackInputs.nixpkgs) lib;
+
+      inputs = tackInputs // {
+        self = self';
+      };
+
+      self' = flakeOutputs // {
+        inherit inputs;
+        inherit (self) outPath;
+      };
+
+      importsList =
+        with lib;
         [
           ./modules
           ./hosts
@@ -12,131 +25,13 @@
         |> map (fileset.fileFilter (file: file.hasExt "nix" && !hasPrefix "_" file.name))
         |> fileset.unions
         |> fileset.toList;
-      _module.args.rootPath = ./.;
-    }
-    |> (eval: { inherit eval; } // eval.config.processedFlake);
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-    flake-file.url = "github:vic/flake-file";
-    base16.url = "github:SenchoPens/base16.nix";
-    birdee = {
-      url = "github:BirdeeHub/nix-wrapper-modules";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    disko = {
-      url = "github:nix-community/disko";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    flake-compat.url = "github:NixOS/flake-compat";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.systems.follows = "systems";
-    };
-    hjem = {
-      url = "github:feel-co/hjem";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    hyprland = {
-      url = "github:hyprwm/Hyprland";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-    lan-mouse = {
-      url = "github:feschber/lan-mouse";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        rust-overlay.follows = "rust-overlay";
-      };
-    };
-    nix-cachyos-kernel = {
-      url = "github:xddxdd/nix-cachyos-kernel/release";
-      inputs = {
-        flake-parts.follows = "flake-parts";
-        flake-compat.follows = "flake-compat";
-      };
-    };
-    nix-index-database = {
-      url = "github:nix-community/nix-index-database";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nixos-core = {
-      url = "github:manic-systems/nixos-core";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nvfetcher = {
-      url = "github:berberman/nvfetcher";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-compat.follows = "flake-compat";
-        flake-utils.follows = "flake-utils";
-      };
-    };
-    oneshill = {
-      url = "git+https://gitea.onelock.org/onelock/oneshill.git?ref=retroid";
-      flake = false;
-    };
-    preservation.url = "github:nix-community/preservation";
-    quickshell = {
-      url = "github:quickshell-mirror/quickshell";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    rust-overlay = {
-      url = "github:oxalica/rust-overlay";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    sops-nix = {
-      url = "github:Mic92/sops-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    spicetify-nix = {
-      url = "github:Gerg-L/spicetify-nix";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        systems.follows = "systems";
-      };
-    };
-    systems.url = "github:nix-systems/x86_64-linux";
-    theme-store = {
-      url = "github:zen-browser/theme-store";
-      flake = false;
-    };
-    treefmt-nix = {
-      url = "github:numtide/treefmt-nix";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    vimmax = {
-      url = "github:onelocked/vimmax";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-parts.follows = "flake-parts";
-        systems.follows = "systems";
-      };
-    };
-    yazi = {
-      url = "github:/sxyazi/yazi";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        flake-utils.follows = "flake-utils";
-        rust-overlay.follows = "rust-overlay";
-      };
-    };
-    yazi-plugins = {
-      url = "github:AminurAlam/yazi-plugins";
-      flake = false;
-    };
-    zen-browser = {
-      url = "github:0xc000022070/zen-browser-flake";
-      inputs = {
-        nixpkgs.follows = "nixpkgs";
-        home-manager.follows = "";
-      };
-    };
-  };
+      flakeOutputs =
+        tackInputs.flake-parts.lib.evalFlakeModule { inherit inputs; } {
+          imports = importsList;
+          _module.args.rootPath = ./.;
+        }
+        |> (eval: { inherit eval; } // eval.config.processedFlake);
+    in
+    flakeOutputs;
 }
