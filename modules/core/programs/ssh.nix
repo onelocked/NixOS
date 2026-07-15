d92cb422bd4cf786312eea5d738d4fc052be92fe
onelocked <180497738+onelocked@users.noreply.github.com>
2026-07-15 18:21:57 +0100

    ssh(fix): persist keys

--- modules/core/programs/ssh.nix
+++ modules/core/programs/ssh.nix
@@ -15,6 +15,17 @@
         (lib.mkIf cfg.enable {
           services.openssh = {
             enable = true;
+            hostKeys = [
+              {
+                path = "/persist/etc/ssh/ssh_host_ed25519_key";
+                type = "ed25519";
+              }
+              {
+                path = "/persist/etc/ssh/ssh_host_rsa_key";
+                type = "rsa";
+                bits = 4096;
+              }
+            ];
             settings = {
               PermitRootLogin = "no";
               PasswordAuthentication = false;
