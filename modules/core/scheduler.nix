3bc62073717f5097d8ccc0f01493152994d8e8ec
onelocked <180497738+onelocked@users.noreply.github.com>
2026-07-23 00:16:54 +0100

    scheduler: switch from lavd -> cake

--- modules/core/scheduler.nix
+++ modules/core/scheduler.nix
@@ -6,7 +6,7 @@
         scx = {
           enable = true;
           package = pkgs.scx.rustscheds;
-          scheduler = "scx_lavd"; # Gaming scheduler https://github.com/sched-ext/scx/blob/main/scheds/rust/README.md
+          scheduler = "scx_cake"; # https://wiki.cachyos.org/configuration/sched-ext/#scx_cake
         };
       };
     };
