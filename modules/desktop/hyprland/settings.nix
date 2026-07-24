09e1fe7c9f176a7adf44336f548b726a1693e743
onelocked <180497738+onelocked@users.noreply.github.com>
2026-07-24 20:55:58 +0100

    hyprland: set static gaps to 30

--- modules/desktop/hyprland/settings.nix
+++ modules/desktop/hyprland/settings.nix
@@ -35,9 +35,7 @@
           hl.config({
             general = {
               gaps_in           = 10,
-              gaps_out = { top = ${
-                if config.forte.quickshell.enable then toString 60 else toString 30
-              }, bottom = 30, left = 30, right = 30 },
+              gaps_out = 30,
               no_focus_fallback = true,
 
               border_size       = 9,
