a65f3feb922d544bd846f3a32a8d7189e1e0d89a
onelocked <180497738+onelocked@users.noreply.github.com>
2026-08-06 23:29:59 +0100

    nvidia-gpu: add vaapi driver

--- hosts/gaming-pc/hardware/nvidia-gpu.nix
+++ hosts/gaming-pc/hardware/nvidia-gpu.nix
@@ -1,24 +1,30 @@
 {
-  exo.hardware.gaming-pc = {
-    hardware.graphics = {
-      enable = true;
-      enable32Bit = true;
-    };
-    hardware.nvidia = {
-      branch = "bleeding_edge";
-      modesetting.enable = true;
-      open = true;
-      nvidiaSettings = false;
-    };
-    services.xserver.videoDrivers = [ "nvidia" ]; # needed to  have nviida drivers enabled
-    forte.allowUnfree = [ "nvidia-x11" ];
+  exo.hardware.gaming-pc =
+    { pkgs, ... }:
+    {
+      hardware.graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+          nvidia-vaapi-driver
+          nv-codec-headers-12
+        ];
+      };
+      hardware.nvidia = {
+        branch = "bleeding_edge";
+        modesetting.enable = true;
+        open = true;
+        nvidiaSettings = false;
+      };
+      services.xserver.videoDrivers = [ "nvidia" ]; # needed to  have nviida drivers enabled
+      forte.allowUnfree = [ "nvidia-x11" ];
 
-    services.lact.enable = true; # GPU fan control GUI with a daemon
+      services.lact.enable = true; # GPU fan control GUI with a daemon
 
-    environment.sessionVariables = {
-      LIBVA_DRIVER_NAME = "nvidia";
-      __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      environment.sessionVariables = {
+        LIBVA_DRIVER_NAME = "nvidia";
+        __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+      };
+      forte.persist.root.directories = [ "/etc/lact" ];
     };
-    forte.persist.root.directories = [ "/etc/lact" ];
-  };
 }
