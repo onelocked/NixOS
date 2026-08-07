52259714181eafe5c7fe82497749b4cff13532d1
onelocked <180497738+onelocked@users.noreply.github.com>
2026-08-07 22:41:04 +0100

    app2unit: fetch directly from github

--- modules/desktop/desktop-specific.nix
+++ modules/desktop/desktop-specific.nix
@@ -1,6 +1,7 @@
 {
+  tack.inputs.fetch.app2unit = "gh:Vladimir-csp/app2unit";
   exo.mods.desktop =
-    { pkgs, ... }:
+    { pkgs, inputs, ... }:
     {
       config = {
         services.ddccontrol.enable = true;
@@ -9,6 +10,13 @@
           wl-clipboard
           ddcutil
           app2unit
+        ];
+        nixpkgs.overlays = [
+          (_: prev: {
+            app2unit = prev.app2unit.overrideAttrs (oldAttrs: {
+              src = inputs.app2unit;
+            });
+          })
         ];
         hj.environment.sessionVariables = {
           APP2UNIT_SLICES = "a=app-graphical.slice b=background-graphical.slice s=session-graphical.slice";
