098827216be3200021a42f009f6ed552cfc20521
onelocked <180497738+onelocked@users.noreply.github.com>
2026-08-31 17:52:18 +0100

    kernel: make xanmod the default, enable zramSwap by default

--- hosts/desktops/gaming-pc.nix
+++ hosts/desktops/gaming-pc.nix
@@ -11,18 +11,9 @@
         neovim
       ];
       extraConfig =
-        {
-          config,
-          pkgs,
-          lib,
-          ...
-        }:
+        { config, lib, ... }:
         {
           sops.defaultSopsFile = ../../.secrets/personal.yaml;
-          forte.flatpak.enable = false;
-
-          boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-
           forte.quickshell.enable = lib.mkForce false;
           services.nfs.server = {
             enable = true;
@@ -77,21 +68,23 @@
           nv-codec-headers-12
         ];
       };
+
       hardware.nvidia = {
         branch = "bleeding_edge";
         modesetting.enable = true;
         open = true;
         nvidiaSettings = false;
       };
+
       services.xserver.videoDrivers = [ "nvidia" ]; # needed to  have nviida drivers enabled
       forte.allowUnfree = [ "nvidia-x11" ];
-
-      services.lact.enable = true; # GPU fan control GUI with a daemon
 
       environment.sessionVariables = {
         LIBVA_DRIVER_NAME = "nvidia";
         __GLX_VENDOR_LIBRARY_NAME = "nvidia";
       };
+      services.lact.enable = true; # GPU fan control GUI with a daemon
+
       forte.persist.root.directories = [ "/etc/lact" ];
     };
 
--- hosts/desktops/mini-pc.nix
+++ hosts/desktops/mini-pc.nix
@@ -11,25 +11,17 @@
         remote-access
       ];
       extraConfig =
+        { lib, ... }:
         {
-          pkgs,
-          config,
-          lib,
-          ...
-        }:
-        {
-          boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
           forte.openssh.enable = lib.mkForce false;
           forte.opkssh.enable = true;
           sops.defaultSopsFile = ../../.secrets/personal.yaml;
-          forte.persist = {
-            home = {
-              directories = [
-                ".ssh"
-                ".local/share/.gnupg"
-              ];
-            };
-          };
+
+          forte.persist.home.directories = [
+            ".ssh"
+            ".local/share/.gnupg"
+          ];
+
           forte.hyprland.lua.settings = # lua
             ''
               hl.monitor({
@@ -78,62 +70,6 @@
               Host *
                 IdentitiesOnly yes
             '';
-          sops.secrets."wg_peer_private_key" = { };
-          sops.secrets."wg_peer_psk" = { };
-          networking.wg-quick.interfaces = {
-            wg0 = {
-              autostart = false;
-              address = [ "10.10.50.4/32" ];
-              postUp = "ip route add 217.182.74.106/32 via 192.168.1.1";
-              preDown = "ip route del 217.182.74.106/32 via 192.168.1.1 || true";
-              dns = [ "1.1.1.1" ];
-              privateKeyFile = config.sops.secrets."wg_peer_private_key".path;
-              peers = [
-                {
-                  publicKey = "isX1x//XqsPSKINt0P6+TQ7V+eTaUJnB3oQqFSzuXzc=";
-                  presharedKeyFile = config.sops.secrets."wg_peer_psk".path;
-                  endpoint = "vps.onelock.org:4738";
-                  allowedIPs = [
-                    "1.0.0.0/8"
-                    "2.0.0.0/8"
-                    "3.0.0.0/8"
-                    "4.0.0.0/6"
-                    "8.0.0.0/7"
-                    "11.0.0.0/8"
-                    "12.0.0.0/6"
-                    "16.0.0.0/4"
-                    "32.0.0.0/3"
-                    "64.0.0.0/2"
-                    "128.0.0.0/3"
-                    "160.0.0.0/5"
-                    "168.0.0.0/6"
-                    "172.0.0.0/12"
-                    "172.32.0.0/11"
-                    "172.64.0.0/10"
-                    "172.128.0.0/9"
-                    "173.0.0.0/8"
-                    "174.0.0.0/7"
-                    "176.0.0.0/4"
-                    "192.0.0.0/9"
-                    "192.128.0.0/11"
-                    "192.160.0.0/13"
-                    "192.169.0.0/16"
-                    "192.170.0.0/15"
-                    "192.172.0.0/14"
-                    "192.176.0.0/12"
-                    "192.192.0.0/10"
-                    "193.0.0.0/8"
-                    "194.0.0.0/7"
-                    "196.0.0.0/6"
-                    "200.0.0.0/5"
-                    "208.0.0.0/4"
-                    "1.1.1.1/32"
-                  ];
-                  persistentKeepalive = 25;
-                }
-              ];
-            };
-          };
         };
     };
   };
@@ -158,7 +94,7 @@
         "amd_pstate"
         "kvm-amd"
       ];
-      boot.kernelParams = [ "amd_pstate=active" ]; # use active mode (dynamic scaling)
+      boot.kernelParams = [ "amd_pstate=active" ];
       hardware.enableRedistributableFirmware = true;
       nixpkgs.config.rocmSupport = true;
       hardware.amdgpu.opencl.enable = true;
@@ -175,6 +111,7 @@
 
       hj.packages = [ self'.packages.amdgpu_top ];
     };
+
   exo.disko.mini-pc = {
     boot.tmp.useTmpfs = true;
     boot.tmp.tmpfsSize = "75%";
